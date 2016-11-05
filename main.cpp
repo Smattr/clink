411b08f07b76e8b0aaa3306bdcf64c65e5f12fa8
Matthew Fernandez <matthew.fernandez@gmail.com>
2016-11-05 16:21:26 -0700

    implement command line parsing

--- main.cpp
+++ main.cpp
@@ -1,5 +1,6 @@
 #include "CXXParser.h"
 #include "Database.h"
+#include <getopt.h>
 #include <iostream>
 #include "Symbol.h"
 #include <unistd.h>
@@ -8,15 +9,55 @@ using namespace std;
 
 static const char default_database[] = ".clink.db";
 
-int main() {
+static void usage(const char *progname) {
+    cerr << "usage: " << progname << " [options]\n"
+         << "\n"
+         << " --file FILE | -f FILE    database to use (.clink.db by default)\n";
+}
+
+/* Default options. */
+static struct {
+    const char *database;
+} opts = {
+    .database = default_database,
+};
+
+static void parse_options(int argc, char **argv) {
+    for (;;) {
+        static const struct option options[] = {
+            {"file", required_argument, 0, 'f'},
+            {0, 0, 0, 0},
+        };
+
+        int index = 0;
+        int c = getopt_long(argc, argv, "f:", options, &index);
+
+        if (c == -1)
+            break;
+
+        switch (c) {
+            case 'f':
+                opts.database = optarg;
+                break;
+
+            default:
+                usage(argv[0]);
+                exit(EXIT_FAILURE);
+        }
+    }
+}
+
+int main(int argc, char **argv) {
+
+    parse_options(argc, argv);
+
     Database db;
-    if (!db.open(default_database)) {
-        cerr << "failed to open " << default_database << "\n";
+    if (!db.open(opts.database)) {
+        cerr << "failed to open " << opts.database << "\n";
         return EXIT_FAILURE;
     }
 
     CPPParser parser;
-
     if (!parser.load("foo.cpp")) {
         cerr << "failed to load foo.cpp\n";
         return EXIT_FAILURE;
