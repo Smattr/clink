033753f2df0aac7e10063f6c49839b8cb6988eef
Matthew Fernandez <matthew.fernandez@gmail.com>
2016-11-06 15:34:44 -0800

    implement database update

--- main.cpp
+++ main.cpp
@@ -1,8 +1,13 @@
+#include <cstdio>
+#include <cstring>
 #include "CXXParser.h"
 #include "Database.h"
+#include <dirent.h>
 #include <getopt.h>
 #include <iostream>
 #include "Symbol.h"
+#include <sys/stat.h>
+#include <sys/types.h>
 #include <unistd.h>
 
 using namespace std;
@@ -69,9 +74,65 @@ static void parse_options(int argc, char **argv) {
     }
 }
 
+static bool ends_with(const char *s, const char *suffix) {
+    size_t s_len = strlen(s);
+    size_t suffix_len = strlen(suffix);
+    return s_len >= suffix_len && strcmp(&s[s_len - suffix_len], suffix) == 0;
+}
+
+static void update(Database &db, CXXParser &parser, time_t era_start,
+        const char *prefix, DIR *directory) {
+    struct dirent entry, *result;
+    while (readdir_r(directory, &entry, &result) == 0 && result != nullptr) {
+
+        // If this entry is a directory, descend and scan for contained files.
+        if (entry.d_type == DT_DIR && strcmp(entry.d_name, ".") &&
+                strcmp(entry.d_name, "..")) {
+            char *path;
+            if (asprintf(&path, "%s%s/", prefix, entry.d_name) < 0)
+                continue;
+            DIR *subdir = opendir(path);
+            if (subdir != nullptr)
+                update(db, parser, era_start, path, subdir);
+            closedir(subdir);
+            free(path);
+
+        // If this entry is a C/C++ file, scan it for relevant things.
+        } else if (entry.d_type == DT_REG && (ends_with(entry.d_name, ".c") ||
+                                              ends_with(entry.d_name, ".cpp") ||
+                                              ends_with(entry.d_name, ".h") ||
+                                              ends_with(entry.d_name, ".hpp"))) {
+            char *path;
+            if (asprintf(&path, "%s%s", prefix, entry.d_name) < 0)
+                continue;
+            struct stat buf;
+            if (stat(path, &buf) < 0 || buf.st_mtime <= era_start) {
+                free(path);
+                continue;
+            }
+            db.purge(path);
+            bool ret = parser.load(path);
+            free(path);
+            if (!ret)
+                continue;
+            parser.process(db);
+            parser.unload();
+        }
+    }
+}
+
 int main(int argc, char **argv) {
 
     parse_options(argc, argv);
+
+    /* Stat the database to figure out when the last update we did was. */
+    time_t era_start;
+    struct stat buf;
+    if (stat(opts.database, &buf) == 0) {
+        era_start = buf.st_mtime;
+    } else {
+        era_start = 0;
+    }
 
     Database db;
     if (!db.open(opts.database)) {
@@ -85,7 +146,28 @@ int main(int argc, char **argv) {
         return EXIT_FAILURE;
     }
 
-    parser.process(db);
+    if (opts.update_database) {
+
+        /* Open a transaction before starting to manipulate the database.
+         * Repeated insertions without a containing transaction are wrapped in
+         * an automatic transaction. Commiting these automatic transactions
+         * intolerably slows the database update.
+         */
+        db.open_transaction();
+
+        /* Scan the directory for files that have changed since the database
+         * was last updated.
+         */
+        DIR *dir = opendir(".");
+        if (dir == nullptr) {
+            cerr << "failed to open directory\n";
+            return EXIT_FAILURE;
+        }
+        update(db, parser, era_start, "", dir);
+        closedir(dir);
+
+        db.close_transaction();
+    }
 
     return EXIT_SUCCESS;
 }
