b56e1533811cab7c1f673f56a546be87f2b3dac2
Matthew Fernandez <matthew.fernandez@gmail.com>
2016-11-10 20:53:03 -0800

    shove Symbol constructor into header

--- Makefile
+++ Makefile
@@ -1,7 +1,7 @@
 # FIXME: Beat libclang sufficiently into submission to replace this with CMake
 
 SOURCES = CXXParser.cpp CXXParser.h Database.cpp Database.h main.cpp Parser.h \
-  Symbol.cpp Symbol.h UI.h UILine.cpp UILine.h
+  Symbol.h UI.h UILine.cpp UILine.h
 
 CXX ?= c++
 CXXFLAGS += -std=c++11 -g -W -Wall -Wextra -O3
--- Symbol.h
+++ Symbol.h
@@ -15,7 +15,10 @@ class Symbol {
 
 public:
     Symbol(const char *name, const char *path, symbol_category_t category,
-        unsigned line, unsigned col, const char *parent, const char *context);
+        unsigned line, unsigned col, const char *parent, const char *context)
+        : m_name(name), m_path(path), m_category(category), m_line(line),
+        m_col(col), m_parent(parent ? parent : ""), m_context(context) {
+    }
 
     const char *name() const { return m_name.c_str(); }
     const char *path() const { return m_path.c_str(); }
