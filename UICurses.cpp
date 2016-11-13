a9cc8b8085f7f113668ef48d1125c661ee0f4490
Matthew Fernandez <matthew.fernandez@gmail.com>
2016-11-12 16:25:14 -0800

    implement outline of result printing

--- UICurses.cpp
+++ UICurses.cpp
@@ -5,6 +5,7 @@
 #include <string>
 #include <string.h>
 #include "UICurses.h"
+#include <vector>
 
 using namespace std;
 
@@ -36,6 +37,76 @@ static unsigned offset_x(unsigned index) {
 static unsigned offset_y(unsigned index) {
     assert(index < functions_sz);
     return LINES - functions_sz + index;
+}
+
+struct ResultRow {
+    vector<string> text;
+    string path;
+    unsigned line;
+    unsigned col;
+};
+
+struct Results {
+    vector<string> headings;
+    vector<ResultRow> rows;
+};
+
+static int print_results(const Results &results, unsigned from_row) {
+    assert(from_row == 0 || from_row < results.rows.size());
+
+    /* The number of rows we can fit is the number of lines on the screen with
+     * some room extracted for the column headings, menu and status.
+     */
+    if ((unsigned)LINES < functions_sz + 2 + 1 + 1)
+        return -1;
+    unsigned row_count = LINES - functions_sz - 2 - 1;
+    if (row_count > results.rows.size() - from_row) {
+        /* Can't show more rows than we have. */
+        row_count = results.rows.size() - from_row;
+    }
+    if (row_count > 62) {
+        /* We only have 62 addressable rows. */
+        row_count = 62;
+    }
+
+    /* Figure out column widths. */
+    vector<unsigned> widths;
+    for (unsigned i = 0; i < results.headings.size(); i++) {
+        /* Find the maximum width of this column's results. */
+        unsigned width = results.headings[i].size();
+        for (unsigned j = from_row; j < from_row + row_count; j++) {
+            assert(j < results.rows.size());
+            assert(i < results.rows[j].text.size());
+            if (results.rows[j].text[i].size() > width)
+                width = results.rows[j].text[i].size();
+        }
+        widths.push_back(width + 1);
+    }
+    
+    /* Print column headings. */
+    move(0, 0);
+    for (unsigned i = 0; i < results.headings.size(); i++) {
+        size_t padding = widths[i] - results.headings[i].size();
+        printw("%s%s", results.headings[i].c_str(),
+            string(padding, ' ').c_str());
+    }
+
+    /* Print the rows. */
+    for (unsigned i = from_row; i < from_row + row_count; i++) {
+        move(i - from_row, 0);
+        for (unsigned j = 0; j < widths.size(); j++) {
+            size_t padding = widths[i] - results.rows[i].text[j].size();
+            printw("%s%s", results.rows[i].text[j].c_str(),
+                string(padding, ' ').c_str());
+        }
+    }
+
+    /* Print footer. */
+    move(row_count + 1, 0);
+    printw("* Lines %u-%u of %u *", from_row, from_row + row_count,
+        results.rows.size());
+
+    return 0;
 }
 
 int UICurses::run(Database &db) {
