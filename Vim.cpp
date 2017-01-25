0ab863ffc00e7e8b34264be9ae7e43c5da2a52b4
Matthew Fernandez <matthew.fernandez@gmail.com>
2017-01-24 19:29:06 -0800

    testing functionality for vim_open

--- Vim.cpp
+++ Vim.cpp
@@ -1,6 +1,8 @@
+#include <iostream>
 #include <cstdlib>
 #include "FauxTerm.h"
 #include <string>
+#include <unistd.h>
 
 using namespace std;
 
@@ -9,3 +11,30 @@ int vim_open(const string &filename, unsigned line, unsigned col) {
         + ")\" " + filename;
     return system(cmd.c_str());
 }
+
+#ifdef TEST_VIM_OPEN
+static void usage(const string &prog) {
+    cerr << "usage: " << prog << " filename line col\n";
+}
+
+int main(int argc, char **argv) {
+    if (argc != 4) {
+        usage(argv[0]);
+        return EXIT_FAILURE;
+    }
+
+    const string filename = argv[1];
+    unsigned line, col;
+
+    try {
+        line = stoul(argv[2]);
+        col = stoul(argv[3]);
+    } catch (exception &e) {
+        cerr << e.what() << "\n";
+        usage(argv[0]);
+        return EXIT_FAILURE;
+    }
+
+    return vim_open(filename, line, col);
+}
+#endif
