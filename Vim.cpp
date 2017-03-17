42e6fc3b58ad83633a6199298162da2d38aa13f1
Matthew Fernandez <matthew.fernandez@gmail.com>
2017-03-16 19:25:15 -0700

    build a standalone vimcat
    Collateral bonus.

--- Vim.cpp
+++ Vim.cpp
@@ -497,17 +497,3 @@ vector<string> vim_highlight(const string &filename) {
 
     return lines;
 }
-
-#ifdef TEST_VIM_HIGHLIGHTER
-int main(int argc, char **argv) {
-    if (argc != 2 || (argc > 1 && strcmp(argv[1], "--help") == 0)) {
-        cerr << "usage: " << argv[0] << " <filename>\n";
-        return EXIT_FAILURE;
-    }
-
-    for (const string &line : vim_highlight(argv[1]))
-        cout << line;
-
-    return EXIT_SUCCESS;
-}
-#endif
