f9362d5d6c9037cff981fdcb68b9c15c39d67b52
Matthew Fernandez <matthew.fernandez@gmail.com>
2017-03-16 19:22:17 -0700

    expose a version of Symbol without context
    The idea here is to move towards parsers yielding their results as SymbolCores,
    which will later have a context appended to them after Vim highlighting.

--- Symbol.h
+++ Symbol.h
@@ -11,40 +11,61 @@ typedef enum {
     ST_RESERVED,
 } symbol_category_t;
 
-class Symbol {
+class SymbolCore {
 
-public:
-    Symbol(const char *name, const char *path, symbol_category_t category,
-        unsigned line, unsigned col, const char *parent, const char *context)
-        : m_name(name), m_path(path), m_category(category), m_line(line),
-        m_col(col), m_parent(parent ? parent : ""), m_context(context) {
-    }
+ public:
+  SymbolCore(const char *name, const char *path, symbol_category_t category,
+    unsigned line, unsigned col, const char *parent)
+    : m_name(name), m_path(path), m_category(category), m_line(line),
+      m_col(col), m_parent(parent ? parent : "") {
+  }
 
-    Symbol(const std::string &name, std::string &path,
-        symbol_category_t category, unsigned line, unsigned col,
-        const char *parent, const char *context)
-        : m_name(name), m_path(path), m_category(category), m_line(line),
-        m_col(col), m_parent(parent ? parent : ""), m_context(context) {
-    }
+  SymbolCore(const std::string &name, std::string &path,
+    symbol_category_t category, unsigned line, unsigned col, const char *parent)
+    : m_name(name), m_path(path), m_category(category), m_line(line),
+      m_col(col), m_parent(parent ? parent : "") {
+  }
 
-    const char *name() const { return m_name.c_str(); }
-    const char *path() const { return m_path.c_str(); }
-    symbol_category_t category() const { return m_category; }
-    unsigned line() const { return m_line; }
-    unsigned col() const { return m_col; }
-    const char *parent() const {
-        return m_parent == "" ? "<global>" : m_parent.c_str();
-    }
-    const char *context() const { return m_context.c_str(); }
+  const char *name() const { return m_name.c_str(); }
+  const char *path() const { return m_path.c_str(); }
+  symbol_category_t category() const { return m_category; }
+  unsigned line() const { return m_line; }
+  unsigned col() const { return m_col; }
+  const char *parent() const {
+    return m_parent == "" ? "<global>" : m_parent.c_str();
+  }
 
-private:
-    std::string m_name;
-    std::string m_path;
-    symbol_category_t m_category;
-    unsigned m_line;
-    unsigned m_col;
-    std::string m_parent;
-    std::string m_context;
+  virtual ~SymbolCore() {}
+
+ protected:
+  std::string m_name;
+  std::string m_path;
+  symbol_category_t m_category;
+  unsigned m_line;
+  unsigned m_col;
+  std::string m_parent;
+
+};
+
+class Symbol : public SymbolCore {
+
+ public:
+  Symbol(const char *name, const char *path, symbol_category_t category,
+    unsigned line, unsigned col, const char *parent, const char *context)
+    : SymbolCore(name, path, category, line, col, parent), m_context(context) {
+  }
+
+  Symbol(const std::string &name, std::string &path, symbol_category_t category,
+    unsigned line, unsigned col, const char *parent, const char *context)
+    : SymbolCore(name, path, category, line, col, parent), m_context(context) {
+  }
+
+  const char *context() const { return m_context.c_str(); }
+
+  virtual ~Symbol() {}
+
+ private:
+  std::string m_context;
 
 };
 
