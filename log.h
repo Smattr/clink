fdb5719df8412529ce9f008295251c55c29c6db5
Matthew Fernandez <matthew.fernandez@gmail.com>
2017-03-22 20:11:05 -0700

    print function name and line number when logging

--- log.h
+++ log.h
@@ -7,6 +7,8 @@ extern FILE *log_file;
 #define LOG(args...) \
   do { \
     if (log_file != nullptr) { \
+      fprintf(log_file, "%s:%d: ", __func__, __LINE__); \
       fprintf(log_file, args); \
+      fprintf(log_file, "\n"); \
     } \
   } while (0)
