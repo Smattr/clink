5e6140385ffac2329235bd9c836d0cad058f1883
Matthew Fernandez <matthew.fernandez@gmail.com>
2017-04-07 16:41:19 -0700

    make logging thread safe
    That was a bit of a whoops.

--- log.cpp
+++ log.cpp
@@ -1,4 +1,8 @@
 #include <cstdio>
 #include "log.h"
+#include <mutex>
+
+using namespace std;
 
 FILE *log_file;
+mutex log_lock;
--- log.h
+++ log.h
@@ -1,12 +1,15 @@
 #pragma once
 
 #include <cstdio>
+#include <mutex>
 
 extern FILE *log_file;
+extern std::mutex log_lock;
 
 #define LOG(args...) \
   do { \
     if (log_file != nullptr) { \
+      std::lock_guard<std::mutex> _guard(log_lock); \
       fprintf(log_file, "%s:%d: ", __func__, __LINE__); \
       fprintf(log_file, args); \
       fprintf(log_file, "\n"); \
