6f82420f365db9bd1ca2247d8f5748a74a518886
Matthew Fernandez <matthew.fernandez@gmail.com>
2020-03-17 12:02:16 -0700

    also implement support for Clang errors

--- libclink/src/error.c
+++ libclink/src/error.c
@@ -1,4 +1,5 @@
 #include <assert.h>
+#include <clang-c/Index.h>
 #include <clink/error.h>
 #include "error.h"
 #include <regex.h>
@@ -11,10 +12,26 @@ int sqlite_error(int err) {
   return -err;
 }
 
-int regex_error(int err) {
-  assert(err < 128 && "unexpected POSIX regex error code");
+int clang_error(int err) {
+  assert(err < 128 && "unexpected Clang error code");
   return err << 8;
 }
+
+int regex_error(int err) {
+  assert(err < 128 && "unexpected POSIX regex error code");
+  return err << 16;
+}
+
+// errors defined by libclang
+static const char *CLANGERRORS[] = {
+  [CXError_Failure]          = "generic error code, no further details are "
+                               "available",
+  [CXError_Crashed]          = "libclang crashed while performing the "
+                               "requested operation",
+  [CXError_InvalidArguments] = "the function detected that the arguments "
+                               "violate the function contract",
+  [CXError_ASTReadError]     = "an AST deserialisation error has occurred",
+};
 
 // errors defined by POSIX regex
 static const char *REGERRORS[] = {
@@ -36,7 +53,8 @@ static const char *REGERRORS[] = {
 
 const char *clink_strerror(int err) {
 
-  // if this is a negative number, it is an encoded SQLite or regex error
+  // if this is a negative number, it is an encoded SQLite, Clang, or regex
+  // error
   if (err < 0) {
     int e = -err;
 
@@ -45,8 +63,18 @@ const char *clink_strerror(int err) {
     if (e <= SQLITE_DONE)
       return sqlite3_errstr(e);
 
-    // otherwise assume it is a POSIX error code, shifted out of the first byte
-    int regex_code = e >> 8;
+    // otherwise, see if it falls in the range we have defined for Clang errors
+    {
+      int clang_code = e >> 8;
+      if (clang_code < 256) {
+        if (clang_code < (int)(sizeof(CLANGERRORS) / sizeof(CLANGERRORS[0])))
+          return CLANGERRORS[clang_code];
+        return "unknown";
+      }
+    }
+
+    // otherwise assume it is a POSIX regex error code, shifted out of the first byte
+    int regex_code = e >> 16;
 
     if (regex_code < (int)(sizeof(REGERRORS) / sizeof(REGERRORS[0])))
       return REGERRORS[regex_code];
--- libclink/src/error.h
+++ libclink/src/error.h
@@ -4,6 +4,10 @@
 __attribute__((visibility("internal")))
 int sqlite_error(int err);
 
+// turn a libclang error code into a Clink-encoded error number
 __attribute__((visibility("internal")))
+int clang_error(int err);
+
 // turn a POSIX regex error code into a Clink-encoded error number
+__attribute__((visibility("internal")))
 int regex_error(int err);
