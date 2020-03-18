b61431bb57a0f19db6de05cc309e3b95d5e56c76
Matthew Fernandez <matthew.fernandez@gmail.com>
2020-03-18 09:51:18 -0700

    implement db_find_definition()

--- libclink/include/clink/db.h
+++ libclink/include/clink/db.h
@@ -27,6 +27,18 @@ int clink_db_open(struct clink_db *db, const char *path);
  */
 int clink_db_remove(struct clink_db *db, const char *path);
 
+/** find definitions of a symbol in a Clink symbol database
+ *
+ * @param db Database to search
+ * @param name Symbol to look for
+ * @param callback This is invoked for each definition of the symbol found.
+ *   Return a non-zero value to terminate the search.
+ * @return 0 on success, a Clink error code or the returned value from the
+ *   caller’s callback on failure
+ */
+int clink_db_find_symbol(struct clink_db *db, const char *name,
+  int (*callback)(const struct clink_result *result));
+
 /** find occurrences of a symbol in a Clink symbol database
  *
  * @param db Database to search
