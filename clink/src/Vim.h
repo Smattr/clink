4f2b2af89757684c35c1df56e0b8e01f28c2b85c
Matthew Fernandez <matthew.fernandez@gmail.com>
2020-03-18 13:38:32 -0700

    remove ported Vim files

--- clink/src/Vim.h
+++
@@ -1,9 +0,0 @@
-#pragma once
-
-#include <cstdio>
-#include <string>
-#include <vector>
-
-int vim_open(const std::string &filename, unsigned long line, unsigned long col);
-
-std::vector<std::string> vim_highlight(const std::string &filename);
