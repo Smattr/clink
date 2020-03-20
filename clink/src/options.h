cc9fa365fbce64c613b0ddd4a8c4bc9313f68975
Matthew Fernandez <matthew.fernandez@gmail.com>
2020-03-19 20:11:38 -0700

    remove some ported options

--- clink/src/options.h
+++ clink/src/options.h
@@ -1,30 +1,5 @@
 #pragma once
 
-#if 0
-#include <string>
-#include <vector>
-
-typedef enum {
-  UI_NONE,
-  UI_CURSES,
-  UI_LINE,
-} ui_t;
-
-struct Options {
-  const char *database;
-  bool update_database;
-  ui_t ui;
-
-  // Parallelism (0 == auto).
-  unsigned long threads;
-
-  // Directories to look in for #include files
-  std::vector<std::string> include_dirs;
-};
-
-extern Options opts;
-#endif
-
 #include <stdbool.h>
 #include <stddef.h>
 
