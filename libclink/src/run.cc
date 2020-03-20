11eabe8cc88add8ec8a3c8e7ca194b86f6759a48
Matthew Fernandez <matthew.fernandez@gmail.com>
2020-03-20 12:31:26 -0700

    libclink: implement functionality to run an external program

---
+++ libclink/src/run.cc
@@ -0,0 +1,87 @@
+#include <assert.h>
+#include <errno.h>
+#include <fcntl.h>
+#include "run.h"
+#include <spawn.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <sys/wait.h>
+#include <unistd.h>
+
+#ifdef __APPLE__
+  #include <crt_externs.h>
+#endif
+
+static char **get_environ(void) {
+#ifdef __APPLE__
+  // on macOS, environ is not directly accessible
+  return *_NSGetEnviron();
+#else
+  // some platforms fail to expose environ in a header (e.g. FreeBSD), so
+  // declare it ourselves and assume it will be available when linking
+  extern char **environ;
+
+  return environ;
+#endif
+}
+
+int run(const char **argv, bool mask_stdout) {
+
+  assert(argv != nullptr);
+
+  int rc = 0;
+  posix_spawn_file_actions_t fa;
+  int devnull = -1;
+
+  rc = posix_spawn_file_actions_init(&fa);
+  if (rc != 0)
+    return rc;
+
+  if (mask_stdout) {
+
+#ifdef __APPLE__
+    devnull = open("/dev/null", O_RDWR);
+#else
+    // On non-Apple platforms we can speed up Vim’s execution by giving it a PTY
+    // instead of /dev/null. It is not clear why, but using /dev/null slows Vim
+    // down somehow. It is also not clear why this PTY strategy does not work on
+    // macOS.
+    devnull = posix_openpt(O_RDWR);
+#endif
+
+    if (devnull < 0) {
+      rc = errno;
+      goto done;
+    }
+
+    // dup /dev/null over standard streams
+    rc = posix_spawn_file_actions_adddup2(&fa, devnull, STDIN_FILENO) ||
+         posix_spawn_file_actions_adddup2(&fa, devnull, STDOUT_FILENO) ||
+         posix_spawn_file_actions_adddup2(&fa, devnull, STDERR_FILENO);
+    if (rc != 0)
+      goto done;
+  }
+
+  {
+    // spawn the child
+    pid_t pid;
+    auto args = const_cast<char*const*>(argv);
+    rc = posix_spawnp(&pid, argv[0], &fa, nullptr, args, get_environ());
+    if (rc != 0)
+      goto done;
+
+    // wait for it to finish executing
+    if (waitpid(pid, &rc, 0) < 0) {
+      rc = errno;
+      goto done;
+    }
+  }
+
+done:
+  // clean up
+  if (devnull != -1)
+    close(devnull);
+  (void)posix_spawn_file_actions_destroy(&fa);
+
+  return rc;
+}
---
+++ libclink/src/run.h
@@ -0,0 +1,10 @@
+#pragma once
+
+/** run a command and return its exit status
+ *
+ * \param argv Argument list of program to run
+ * \param mask_stdout Hide the process’ standard output and error?
+ * \returns The process’ exit status or an errno
+ */
+__attribute__((visibility("internal")))
+int run(const char **argv, bool mask_stdout);
