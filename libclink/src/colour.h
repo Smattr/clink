5d9b738108f6d3c9916a01a54d163368aa01626f
Matthew Fernandez <matthew.fernandez@gmail.com>
2020-03-20 12:50:01 -0700

    libclink: port html colour mapping functionality

---
+++ libclink/src/colour.h
@@ -0,0 +1,15 @@
+#pragma once
+
+#include <cstddef>
+
+namespace clink {
+
+/** convert a 6-character HTML hex colour into its ANSI code equivalent
+ *
+ * \param html String of at least 6 hex characters representing an HTML colour
+ * \returns the closest ANSI colour code
+ */
+__attribute__((visibility("internal")))
+unsigned html_colour_to_ansi(const char *html);
+
+}
