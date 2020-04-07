8196f0d85e0ead68d8b69d4a59ae62fddb83bc36
Matthew Fernandez <matthew.fernandez@gmail.com>
2020-04-06 18:41:46 -0700

    clink: remove now no longer used ends_with()

--- clink/src/ends_with.cc
+++
@@ -1,9 +0,0 @@
-#include <cstddef>
-#include <cstring>
-#include "ends_with.h"
-
-bool ends_with(const char *s, const char *suffix) {
-  size_t s_len = strlen(s);
-  size_t suffix_len = strlen(suffix);
-  return s_len >= suffix_len && strcmp(&s[s_len - suffix_len], suffix) == 0;
-}
--- clink/src/ends_with.h
+++
@@ -1,5 +0,0 @@
-#pragma once
-
-#include <cstddef>
-
-bool ends_with(const char *s, const char *suffix);
