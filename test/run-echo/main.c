f9e50b2a1403d154763a1546bcb9e0ba536f0657
Matthew Fernandez <matthew.fernandez@gmail.com>
2020-04-08 17:16:57 -0700

    add run(echo) test

---
+++ test/run-echo/main.c
@@ -0,0 +1,69 @@
+// a test that running `echo hello world` via libclink’s run() function does
+// what we expect
+
+// force assertions on
+#ifdef NDEBUG
+  #undef NDEBUG
+#endif
+
+#include <assert.h>
+#include "run.h"
+#include <stdlib.h>
+#include <string.h>
+#include <sys/wait.h>
+#include <unistd.h>
+
+int main(void) {
+
+  // create a pipe for the child’s stdout
+  int fd[2];
+  {
+    int r = pipe(fd);
+    assert(r == 0);
+  }
+
+  // create a child whose stdout we can dup over
+  pid_t p = fork();
+  assert(p >= 0);
+
+  if (p == 0) { // the child
+
+    // replace our stdout with the write end of the pipe
+    {
+      int r = dup2(fd[1], STDOUT_FILENO);
+      assert(r >= 0);
+    }
+
+    // use run() to echo some text
+    {
+      const char *args[] = { "echo", "hello", "world", NULL };
+      int r = run(args, false);
+      assert(r == EXIT_SUCCESS);
+    }
+
+    exit(EXIT_SUCCESS);
+
+  }
+
+  // close the end of the pipe we do not need
+  (void)close(fd[1]);
+
+  // wait for the child to exit
+  {
+    int status;
+    pid_t child = wait(&status);
+    assert(child == p);
+    assert(WIFEXITED(status));
+    assert(WEXITSTATUS(status) == EXIT_SUCCESS);
+  }
+
+  // read out of the pipe
+  {
+    char buffer[128] = { 0 };
+    ssize_t r = read(fd[0], buffer, sizeof(buffer));
+    assert(r == strlen("hello world\n"));
+    assert(strcmp(buffer, "hello world\n") == 0);
+  }
+
+  return 0;
+}
