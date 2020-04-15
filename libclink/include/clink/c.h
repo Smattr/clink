43879f4c76c39a168b8907e2e9ecca7c64ac5cb2
Matthew Fernandez <matthew.fernandez@gmail.com>
2020-04-14 21:38:07 -0700

    implement functionality for retrieving compiler's #includes

--- libclink/include/clink/c.h
+++ libclink/include/clink/c.h
@@ -18,6 +18,19 @@ extern "C" {
 int clink_parse_c(clink_iter_t **it, const char *filename, size_t argc,
   const char **argv);
 
+/** get the built-in list of #include paths the compiler knows
+ *
+ * If you pass NULL as the compiler path, this will default to the environment
+ * variable $CXX or "c++" if the environment variable is unset.
+ *
+ * \param compiler Path or command name of the compiler
+ * \param includes [out] List of #include paths on success
+ * \param includes_len [out] Number of elements stored to includes
+ * \returns 0 on success or an errno on failure
+ */
+int clink_compiler_includes(const char *compiler, char ***includes,
+  size_t *includes_len);
+
 #ifdef __cplusplus
 }
 #endif
