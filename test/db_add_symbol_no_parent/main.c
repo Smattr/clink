873ebd6cd47ab7af62ec1d2cd9997de9ac6c9a00
Matthew Fernandez <matthew.fernandez@gmail.com>
2020-04-18 15:28:36 -0700

    add a test of adding a symbol with no parent

---
+++ test/db_add_symbol_no_parent/main.c
@@ -0,0 +1,77 @@
+// basic test of clink_db_add_symbol()
+
+// force assertions on
+#ifdef NDEBUG
+  #undef NDEBUG
+#endif
+
+#include <assert.h>
+#include <clink/db.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+
+int main(void) {
+
+  // find where we should be creating temporary files
+  const char *tmp = getenv("TMPDIR");
+  if (tmp == NULL)
+    tmp = "/tmp";
+
+  // construct a temporary path template
+  char *path = NULL;
+  {
+    int r = asprintf(&path, "%s/tmp.XXXXXX", tmp);
+    assert(r >= 0);
+  }
+
+  // create a temporary directory to work in
+  {
+    char *r = mkdtemp(path);
+    assert(r != NULL);
+  }
+
+  // construct a path within the temporary directory
+  char *target = NULL;
+  {
+    int r = asprintf(&target, "%s/target", path);
+    assert(r >= 0);
+  }
+
+  // open it as a database
+  clink_db_t *db = NULL;
+  int rc = clink_db_open(&db, target);
+  if (rc)
+    fprintf(stderr, "clink_db_open: %s\n", strerror(rc));
+
+  // add a new symbol
+  if (rc == 0) {
+
+    clink_symbol_t symbol
+      = { .category = CLINK_DEFINITION, .lineno = 42, .colno = 10 };
+
+    symbol.name = strdup("sym-name");
+    assert(symbol.name != NULL);
+
+    symbol.path = strdup("/foo/bar");
+    assert(symbol.path != NULL);
+
+    rc = clink_db_add_symbol(db, &symbol);
+    if (rc)
+      fprintf(stderr, "clink_db_add_symbol: %s\n", strerror(rc));
+  }
+
+  // close the database
+  if (rc == 0)
+    clink_db_close(&db);
+
+  // clean up
+  (void)unlink(target);
+  (void)rmdir(path);
+
+  // confirm that the database was opened correctly
+  assert(rc == 0);
+
+  return 0;
+}
