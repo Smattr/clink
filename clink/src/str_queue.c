0640c0198ebbeb43bc40c6ea67495c2a9546ee69
Matthew Fernandez <matthew.fernandez@gmail.com>
2020-04-19 13:44:35 -0700

    add an implementation of a simple queue

---
+++ clink/src/str_queue.c
@@ -0,0 +1,126 @@
+#include <errno.h>
+#include "set.h"
+#include "str_queue.h"
+#include <stdlib.h>
+#include <string.h>
+
+/// a node within the linked list that forms the queue itself
+typedef struct node {
+  char *value;
+  struct node *next;
+} node_t;
+
+struct str_queue {
+
+  /// strings we have previously added to the queue
+  set_t *seen;
+
+  /// head and tail of the queue itself
+  node_t *head;
+  node_t *tail;
+};
+
+int str_queue_new(str_queue_t **sq) {
+
+  if (sq == NULL)
+    return EINVAL;
+
+  str_queue_t *q = calloc(1, sizeof(*q));
+  if (q == NULL)
+    return ENOMEM;
+
+  int rc = 0;
+
+  if ((rc = set_new(&q->seen)))
+    goto done;
+
+done:
+  if (rc) {
+    free(q);
+  } else {
+    *sq = q;
+  }
+
+  return rc;
+}
+
+int str_queue_push(str_queue_t *sq, const char *str) {
+
+  if (sq == NULL)
+    return EINVAL;
+
+  if (str == NULL)
+    return EINVAL;
+
+  // check if we have already seen this string
+  int rc = set_add(sq->seen, str);
+  if (rc)
+    return rc;
+
+  node_t *n = calloc(1, sizeof(*n));
+  if (n == NULL)
+    return ENOMEM;
+
+  n->value = strdup(str);
+  if (n->value == NULL) {
+    free(n);
+    return ENOMEM;
+  }
+
+  if (sq->tail == NULL) {
+    sq->head = sq->tail = n;
+  } else {
+    sq->tail->next = n;
+    sq->tail = n;
+  }
+
+  return 0;
+}
+
+int str_queue_pop(str_queue_t *sq, char **str) {
+
+  if (sq == NULL)
+    return EINVAL;
+
+  if (str == NULL)
+    return EINVAL;
+
+  // is the queue empty?
+  if (sq->head == NULL) {
+    *str = NULL;
+    return 0;
+  }
+
+  *str = sq->head->value;
+
+  // remove the head
+  node_t *head = sq->head;
+  sq->head = sq->head->next;
+  free(head);
+
+  // if the queue only had one element, we need to blank the tail too
+  if (head == sq->tail)
+    sq->tail = NULL;
+
+  return 0;
+}
+
+void str_queue_free(str_queue_t **sq) {
+
+  if (sq == NULL || *sq == NULL)
+    return;
+
+  for (node_t *n = (*sq)->head; n != NULL; ) {
+    node_t *next = n->next;
+    free(n->value);
+    free(n);
+    n = next;
+  }
+  (*sq)->head = NULL;
+  (*sq)->tail = NULL;
+
+  set_free(&(*sq)->seen);
+
+  free(*sq);
+  *sq = NULL;
+}
---
+++ clink/src/str_queue.h
@@ -0,0 +1,36 @@
+// implementation of a string-containing queue
+
+#pragma once
+
+/// opaque pointer to a queue structure
+typedef struct str_queue str_queue_t;
+
+/** create a new queue
+ *
+ * \param sq [out] Created queue on success
+ * \returns 0 on success or an errno on failure
+ */
+int str_queue_new(str_queue_t **sq);
+
+/** add a new string to queue
+ *
+ * \param sq Queue to operate on
+ * \param str String to add
+ * \returns 0 on success if the string was added, EALREADY if the string had
+ *   already been in the queue previously, or another errno on failure
+ */
+int str_queue_push(str_queue_t *sq, const char *str);
+
+/** remove a string from the head of the queue
+ *
+ * \param sq Queue to operate on
+ * \param str [out] String that was popped, or NULL if the queue was empty
+ * \returns 0 on success or an errno on failure
+ */
+int str_queue_pop(str_queue_t *sq, char **str);
+
+/** clear and deallocate a queue
+ *
+ * \param sq Queue to destroy
+ */
+void str_queue_free(str_queue_t **sq);
