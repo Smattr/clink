76ea2546e0fa64d90c9ea45ff4027146219c6c6b
Matthew Fernandez <matthew.fernandez@gmail.com>
2020-04-23 19:04:53 -0700

    clink: make join() more robust

--- clink/src/join.c
+++ clink/src/join.c
@@ -2,24 +2,36 @@
 #include "path.h"
 #include <stddef.h>
 #include <stdio.h>
+#include <string.h>
 
 int join(const char *branch, const char *stem, char **path) {
 
   if (branch == NULL)
     return EINVAL;
 
+  if (strcmp(branch, "") == 0)
+    return EINVAL;
+
   if (stem == NULL)
+    return EINVAL;
+
+  if (strcmp(stem, "") == 0)
     return EINVAL;
 
   if (path == NULL)
     return EINVAL;
 
-  if (is_root(branch)) {
-    if (asprintf(path, "/%s", stem) < 0)
-      return errno;
-  }
+  // determine the prefix, stripping any trailing /s
+  size_t prefix_len = strlen(branch);
+  while (prefix_len > 0 && branch[prefix_len - 1] == '/')
+    --prefix_len;
 
-  if (asprintf(path, "%s/%s", branch, stem) < 0)
+  // determing the suffix, stripping any leading /s
+  const char *suffix = stem;
+  while (*suffix == '/')
+    ++suffix;
+
+  if (asprintf(path, "%.*s/%s", (int)prefix_len, branch, suffix) < 0)
     return errno;
 
   return 0;
