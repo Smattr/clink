6dbcdc960d3683fa8fe2d63f4c4b5839de8e89d2
Matthew Fernandez <matthew.fernandez@gmail.com>
2020-04-23 17:29:20 -0700

    clink: implement is_root() using realpath() instead
    This neatly takes care of duplicate slashes and stray "/./" in paths.

--- clink/src/is_root.c
+++ clink/src/is_root.c
@@ -1,6 +1,7 @@
+#include <limits.h>
 #include "path.h"
 #include <stdbool.h>
-#include <stddef.h>
+#include <stdlib.h>
 #include <string.h>
 
 bool is_root(const char *path) {
@@ -8,5 +9,11 @@ bool is_root(const char *path) {
   if (path == NULL)
     return false;
 
-  return strcmp(path, "/") == 0;
+  // the root directory should be accessible to us, so if this is the root we
+  // should be able to resolve it with realpath()
+  char resolved[PATH_MAX];
+  if (realpath(path, resolved) == NULL)
+    return false;
+
+  return strcmp(resolved, "/") == 0;
 }
