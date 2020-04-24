bbe877ef0552352f6d9cac9893af986f87d326a5
Matthew Fernandez <matthew.fernandez@gmail.com>
2020-04-23 21:00:23 -0700

    clink: remove abspath()
    It turns out we only ever need to form the absolute path of files and
    directories that exist, so we can simply call realpath().

--- clink/src/path.c
+++ clink/src/path.c
@@ -8,37 +8,6 @@
 #include <sys/stat.h>
 #include <unistd.h>
 
-int abspath(const char *path, char **result) {
-
-  if (path == NULL)
-    return EINVAL;
-
-  if (result == NULL)
-    return EINVAL;
-
-  // if the path is already absolute, we just need to duplicate it
-  if (path[0] == '/') {
-    *result = strdup(path);
-    if (*result == NULL)
-      return ENOMEM;
-    return 0;
-  }
-
-  // otherwise, we need to prepend the current working directory
-
-  char *wd = NULL;
-  int rc = cwd(&wd);
-  if (rc)
-    return rc;
-
-  int r = asprintf(result, "%s/%s", wd, path);
-  if (r < 0)
-    rc = errno;
-
-  free(wd);
-  return rc;
-}
-
 int cwd(char **wd) {
 
   if (wd == NULL)
--- clink/src/path.h
+++ clink/src/path.h
@@ -4,14 +4,6 @@
 
 #include <stdbool.h>
 
-/** make a given (possibly relative) path absolute
- *
- * \param path Path to make absolute
- * \param result [out] Absolute path on success
- * \returns 0 on success or an errno on failure
- */
-int abspath(const char *path, char **result);
-
 /** get current working directory
  *
  * \param wd [out] Path to current working directory on success
