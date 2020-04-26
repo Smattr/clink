bdea9ebcfc5cebe82d21ccebfd6aff6600f0b753
Matthew Fernandez <matthew.fernandez@gmail.com>
2020-04-26 09:00:00 -0700

    libclink: simplify parse_asm() to use a no-lookahead iterator

--- libclink/src/parse_asm.c
+++ libclink/src/parse_asm.c
@@ -89,97 +89,61 @@ typedef struct {
   /// optional function that we are currently within during parsing
   char *parent;
 
-  /// possibly valid next symbols to yield
-  clink_symbol_t next[2];
-  bool next_valid[2];
-
-  /// whether we previously yielded next[0]
-  bool head_yielded;
+  /// last yielded symbol
+  clink_symbol_t last;
 
 } state_t;
 
-static bool has_next(const clink_iter_t *it) {
-
-  if (it == NULL)
-    return false;
-
-  const state_t *s = it->state;
-
-  if (s == NULL)
-    return false;
-
-  // do we have a valid next symbol to yield?
-  return (!s->head_yielded && s->next_valid[0]) || s->next_valid[1];
-}
-
-/// add a symbol to our pending next-to-yield list
+/// add a symbol to our pending next-to-yield slot
 static int add_symbol(state_t *s, clink_category_t cat, const char *line,
     const regmatch_t *m, const char *parent) {
 
   assert(s != NULL);
-  assert(!s->next_valid[1]);
   assert(line != NULL);
   assert(m != NULL);
 
-  // which slot are we adding into?
-  clink_symbol_t *sym = s->next_valid[0] ? &s->next[1] : &s->next[0];
-  bool *valid         = s->next_valid[0] ? &s->next_valid[1] : &s->next_valid[0];
-
-  assert(!*valid);
-
   int rc = 0;
 
-  sym->category = cat;
+  s->last.category = cat;
 
-  sym->name = strndup(line + m->rm_so, m->rm_eo - m->rm_so);
-  if (sym->name == NULL) {
+  s->last.name = strndup(line + m->rm_so, m->rm_eo - m->rm_so);
+  if (s->last.name == NULL) {
     rc = ENOMEM;
     goto done;
   }
 
-  sym->path = strdup(s->filename);
-  if (sym->path == NULL) {
+  s->last.path = strdup(s->filename);
+  if (s->last.path == NULL) {
     rc = ENOMEM;
     goto done;
   }
 
-  sym->lineno = s->lineno;
+  s->last.lineno = s->lineno;
 
-  sym->colno = m->rm_so + 1;
+  s->last.colno = m->rm_so + 1;
 
   if (parent != NULL) {
-    sym->parent = strdup(parent);
-    if (sym->parent == NULL) {
+    s->last.parent = strdup(parent);
+    if (s->last.parent == NULL) {
       rc = ENOMEM;
       goto done;
     }
   }
 
-  *valid = true;
-
 done:
   if (rc)
-    clink_symbol_clear(sym);
+    clink_symbol_clear(&s->last);
 
   return rc;
 }
 
-static int move_next(state_t *s) {
+/// populate s->last with the next-to-yield symbol
+static int refill_last(state_t *s) {
 
   assert(s != NULL);
 
-  // discard the head if necessary
-  if (s->next_valid[0]) {
-    clink_symbol_clear(&s->next[0]);
-    s->next_valid[0] = false;
-    s->head_yielded = false;
-  }
-
-  // shuffle the queue forwards
-  s->next[0] = s->next[1];
-  memset(&s->next[1], 0, sizeof(s->next[1]));
-  s->next_valid[0] = s->next_valid[1];
-  s->next_valid[1] = false;
+  // discard the previous if necessary
+  clink_symbol_clear(&s->last);
 
   // now we want to find at least one new symbol
 
@@ -187,13 +151,18 @@ static int move_next(state_t *s) {
 
   char *line = NULL;
   size_t line_size = 0;
-  while (!s->next_valid[1]) {
+  for (;;) {
 
     // update position in file
     ++s->lineno;
 
+    errno = 0;
     if (getline(&line, &line_size, s->in) < 0) {
-      rc = errno;
+      if (errno) {
+        rc = errno;
+      } else { // EOF
+        rc = ENOMSG;
+      }
       break;
     }
 
@@ -203,12 +172,11 @@ static int move_next(state_t *s) {
       size_t m_len = sizeof(m) / sizeof(m[0]);
       int r = regexec(&s->define, line, m_len, m, 0);
       if (r == 0) { // match
-        if ((rc = add_symbol(s, CLINK_DEFINITION, line, &m[1], NULL)))
-          goto done;
-        continue;
+        rc = add_symbol(s, CLINK_DEFINITION, line, &m[1], NULL);
+        break;
       } else if (r != REG_NOMATCH) { // error
         rc = re_err_to_errno(r);
-        goto done;
+        break;
       }
     }
 
@@ -223,12 +191,11 @@ static int move_next(state_t *s) {
         ++m[1].rm_so;
         --m[1].rm_eo;
 
-        if ((rc = add_symbol(s, CLINK_INCLUDE, line, &m[1], NULL)))
-          goto done;
-        continue;
+        rc = add_symbol(s, CLINK_INCLUDE, line, &m[1], NULL);
+        break;
       } else if (r != REG_NOMATCH) { // error
         rc = re_err_to_errno(r);
-        goto done;
+        break;
       }
     }
 
@@ -239,20 +206,18 @@ static int move_next(state_t *s) {
       int r = regexec(&s->function, line, m_len, m, 0);
       if (r == 0) { // match
         if ((rc = add_symbol(s, CLINK_DEFINITION, line, &m[1], NULL)))
-          goto done;
+          break;
 
         // save the context we  are now assumed to be within
         free(s->parent);
         s->parent = strndup(line + m[1].rm_so, m[1].rm_eo - m[1].rm_so);
-        if (s->parent == NULL) {
+        if (s->parent == NULL)
           rc = ENOMEM;
-          goto done;
-        }
 
-        continue;
+        break;
       } else if (r != REG_NOMATCH) { // error
         rc = re_err_to_errno(r);
-        goto done;
+        break;
       }
     }
 
@@ -262,23 +227,21 @@ static int move_next(state_t *s) {
       size_t m_len = sizeof(m) / sizeof(m[0]);
       int r = regexec(&s->call, line, m_len, m, 0);
       if (r == 0) { // match
-        if ((rc = add_symbol(s, CLINK_FUNCTION_CALL, line, &m[2], s->parent)))
-          goto done;
-        continue;
+        rc = add_symbol(s, CLINK_FUNCTION_CALL, line, &m[2], s->parent);
+        break;
       } else if (r != REG_NOMATCH) { // error
         rc = re_err_to_errno(r);
-        goto done;
+        break;
       }
     }
   }
 
-done:
   free(line);
 
   return rc;
 }
 
-static int next(clink_iter_t *it, const clink_symbol_t **yielded) {
+static int next(no_lookahead_iter_t *it, const clink_symbol_t **yielded) {
 
   if (it == NULL)
     return EINVAL;
@@ -290,30 +253,17 @@ static int next(clink_iter_t *it, const clink_symbol_t **yielded) {
   if (s == NULL)
     return EINVAL;
 
-  if (!has_next(it))
-    return EINVAL;
+  // acquire a new next symbol
+  int rc = refill_last(s);
+  if (rc)
+    return rc;
 
-  // if we previously yielded the head, we need to shuffle the pending elements
-  // forwards
-  if (s->head_yielded) {
-
-    assert(s->next_valid[0]);
-
-    int rc = move_next(s);
-    if (rc)
-      return rc;
-  }
-
-  // now we should have an unyielded head
-  assert(s->next_valid[0] && !s->head_yielded);
-
-  *yielded = &s->next[0];
-  s->head_yielded = true;
-
+  // yield this
+  *yielded = &s->last;
   return 0;
 }
 
-static void my_free(clink_iter_t *it) {
+static void my_free(no_lookahead_iter_t *it) {
 
   if (it == NULL)
     return;
@@ -322,11 +272,7 @@ static void my_free(clink_iter_t *it) {
   if (s == NULL)
     return;
 
-  for (size_t i = 0; i < sizeof(s->next) / sizeof(s->next[0]); ++i) {
-    clink_symbol_clear(&s->next[i]);
-    s->next_valid[i] = false;
-  }
-  s->head_yielded = false;
+  clink_symbol_clear(&s->last);
 
   free(s->parent);
   s->parent = NULL;
@@ -366,13 +312,14 @@ int clink_parse_asm(clink_iter_t **it, const char *filename) {
   if (filename == NULL)
     return EINVAL;
 
-  // allocate a new iterator
-  clink_iter_t *i = calloc(1, sizeof(*i));
+  clink_iter_t *wrapper = NULL;
+
+  // allocate a new no-lookahead iterator
+  no_lookahead_iter_t *i = calloc(1, sizeof(*i));
   if (i == NULL)
     return ENOMEM;
 
   // setup our member functions
-  i->has_next = has_next;
   i->next_symbol = next;
   i->free = my_free;
 
@@ -428,15 +375,16 @@ int clink_parse_asm(clink_iter_t **it, const char *filename) {
   }
   s->call_valid = true;
 
-  // advance the iterator to the first symbol(s)
-  if ((rc = move_next(s)))
+  // create a 1-lookahead adapter to wrap this
+  if ((rc = iter_new(&wrapper, i)))
     goto done;
 
 done:
   if (rc) {
-    clink_iter_free(&i);
+    no_lookahead_iter_free(&i);
+    clink_iter_free(&wrapper);
   } else {
-    *it = i;
+    *it = wrapper;
   }
 
   return rc;
