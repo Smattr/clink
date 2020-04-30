233e1caf9af17d7cf6fecdc634e50f6c84f5926f
Matthew Fernandez <matthew.fernandez@gmail.com>
2020-04-29 19:10:37 -0700

    port line_ui

--- clink/src/line_ui.h
+++ clink/src/line_ui.h
@@ -1,9 +1,14 @@
 #pragma once
 
-#include <cstddef>
 #include <clink/clink.h>
 
-// Line-oriented interface. This interface is intended to behave like Cscope’s
-// line-oriented interface for the purpose of playing nice with Vim’s Cscope
-// support. It is not really intended to be used by a human.
-int line_ui(clink::Database &db);
+/** run Clink’s line-oriented interface
+ *
+ * This interface is intended to behave like Cscope’s line-oriented interface
+ * for the purpose of playing nice with Vim’s Cscope support. It is not really
+ * intended to be used by a human.
+ *
+ * \param db Database for searching
+ * \returns 0 on success or an errno on failure
+ */
+int line_ui(clink_db_t *db);
