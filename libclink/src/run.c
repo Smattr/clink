9e4956bea02478b8257af19ac2f6aac85b9571a5
Matthew Fernandez <matthew.fernandez@gmail.com>
2021-08-07 08:37:10 -0700

    run: rename PTY file descriptors
    Using “devnull” in the name of these descriptors was a little confusing when,
    for most of their lifetime, they did not point to /dev/null.
    
    Github: related to #6 “slow Vim cat on non-Linux”

--- libclink/src/run.c
+++ libclink/src/run.c
@@ -20,8 +20,8 @@ int run(const char **argv, bool mask_stdout) {
   // A pair of descriptors to the controlling and subordinate ends of a PTY for
   // interacting with the child. If we are not using a PTY, only the subordinate
   // gets used.
-  int ctrl_devnull = -1;
-  int sub_devnull = -1;
+  int ctrl_pty = -1;
+  int sub_pty = -1;
 
   rc = posix_spawn_file_actions_init(&fa);
   if (rc != 0)
@@ -33,28 +33,28 @@ int run(const char **argv, bool mask_stdout) {
     // It is not clear why, but using /dev/null slows Vim down somehow.
 
     // create a new PTY
-    ctrl_devnull = posix_openpt(O_RDWR|O_NOCTTY);
-    if (ctrl_devnull == -1) {
+    ctrl_pty = posix_openpt(O_RDWR|O_NOCTTY);
+    if (ctrl_pty == -1) {
       rc = errno;
       goto done;
     }
 
     // make the PTY usable
-    if (grantpt(ctrl_devnull) < 0 || unlockpt(ctrl_devnull) < 0) {
+    if (grantpt(ctrl_pty) < 0 || unlockpt(ctrl_pty) < 0) {
       rc = errno;
       goto done;
     }
 
     // open the subordinate end that we will give to the child process
     {
-      const char *sub_name = ptsname(ctrl_devnull);
+      const char *sub_name = ptsname(ctrl_pty);
       if (sub_name == NULL) {
         rc = errno;
         goto done;
       }
-      sub_devnull = open(sub_name, O_RDWR|O_NOCTTY);
+      sub_pty = open(sub_name, O_RDWR|O_NOCTTY);
     }
-    if (sub_devnull < 0) {
+    if (sub_pty < 0) {
       rc = errno;
       goto done;
     }
@@ -63,17 +63,17 @@ int run(const char **argv, bool mask_stdout) {
     // the child emits
     {
       int devnull = open("/dev/null", O_RDWR);
-      if (devnull < 0 || dup2(devnull, ctrl_devnull) < 0) {
+      if (devnull < 0 || dup2(devnull, ctrl_pty) < 0) {
         rc = errno;
         goto done;
       }
-      ctrl_devnull = devnull;
+      ctrl_pty = devnull;
     }
 
     // dup /dev/null over standard streams
-    rc = posix_spawn_file_actions_adddup2(&fa, sub_devnull, STDIN_FILENO) ||
-         posix_spawn_file_actions_adddup2(&fa, sub_devnull, STDOUT_FILENO) ||
-         posix_spawn_file_actions_adddup2(&fa, sub_devnull, STDERR_FILENO);
+    rc = posix_spawn_file_actions_adddup2(&fa, sub_pty, STDIN_FILENO) ||
+         posix_spawn_file_actions_adddup2(&fa, sub_pty, STDOUT_FILENO) ||
+         posix_spawn_file_actions_adddup2(&fa, sub_pty, STDERR_FILENO);
     if (rc != 0)
       goto done;
   }
@@ -95,10 +95,10 @@ int run(const char **argv, bool mask_stdout) {
 
 done:
   // clean up
-  if (sub_devnull != -1)
-    close(sub_devnull);
-  if (ctrl_devnull != -1)
-    close(ctrl_devnull);
+  if (sub_pty != -1)
+    close(sub_pty);
+  if (ctrl_pty != -1)
+    close(ctrl_pty);
   (void)posix_spawn_file_actions_destroy(&fa);
 
   return rc;
