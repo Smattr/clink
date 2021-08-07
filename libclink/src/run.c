b211349117d22a4806eb8606607ac9fd03dd26f3
Matthew Fernandez <matthew.fernandez@gmail.com>
2021-08-07 08:20:38 -0700

    run: split the PTY, giving the child the subordinate end
    The prior code was giving the child the controlling end of the PTY. Technically
    there is nothing wrong with this, but it is somewhat backwards. This change is a
    steps towards solving a further problem, that we are not connecting the other
    end of the PTY to anything. This appears to be why this technique results in a
    deadlock on macOS.
    
    Github: related to #6 “slow Vim cat on non-Linux”

--- libclink/src/run.c
+++ libclink/src/run.c
@@ -16,7 +16,12 @@ int run(const char **argv, bool mask_stdout) {
 
   int rc = 0;
   posix_spawn_file_actions_t fa;
-  int devnull = -1;
+
+  // A pair of descriptors to the controlling and subordinate ends of a PTY for
+  // interacting with the child. If we are not using a PTY, only the subordinate
+  // gets used.
+  int ctrl_devnull = -1;
+  int sub_devnull = -1;
 
   rc = posix_spawn_file_actions_init(&fa);
   if (rc != 0)
@@ -25,30 +30,44 @@ int run(const char **argv, bool mask_stdout) {
   if (mask_stdout) {
 
 #ifdef __APPLE__
-    devnull = open("/dev/null", O_RDWR);
+    sub_devnull = open("/dev/null", O_RDWR);
 #else
     // On non-Apple platforms we can speed up Vim’s execution by giving it a PTY
     // instead of /dev/null. It is not clear why, but using /dev/null slows Vim
     // down somehow. It is also not clear why this PTY strategy does not work on
     // macOS.
-    devnull = posix_openpt(O_RDWR|O_NOCTTY);
-
-    // make the PTY usable
-    if (grantpt(devnull) < 0 || unlockpt(devnull) < 0) {
+    ctrl_devnull = posix_openpt(O_RDWR|O_NOCTTY);
+    if (ctrl_devnull == -1) {
       rc = errno;
       goto done;
     }
+
+    // make the PTY usable
+    if (grantpt(ctrl_devnull) < 0 || unlockpt(ctrl_devnull) < 0) {
+      rc = errno;
+      goto done;
+    }
+
+    // open the subordinate end that we will give to the child process
+    {
+      const char *sub_name = ptsname(ctrl_devnull);
+      if (sub_name == NULL) {
+        rc = errno;
+        goto done;
+      }
+      sub_devnull = open(sub_name, O_RDWR|O_NOCTTY);
+    }
 #endif
 
-    if (devnull < 0) {
+    if (sub_devnull < 0) {
       rc = errno;
       goto done;
     }
 
     // dup /dev/null over standard streams
-    rc = posix_spawn_file_actions_adddup2(&fa, devnull, STDIN_FILENO) ||
-         posix_spawn_file_actions_adddup2(&fa, devnull, STDOUT_FILENO) ||
-         posix_spawn_file_actions_adddup2(&fa, devnull, STDERR_FILENO);
+    rc = posix_spawn_file_actions_adddup2(&fa, sub_devnull, STDIN_FILENO) ||
+         posix_spawn_file_actions_adddup2(&fa, sub_devnull, STDOUT_FILENO) ||
+         posix_spawn_file_actions_adddup2(&fa, sub_devnull, STDERR_FILENO);
     if (rc != 0)
       goto done;
   }
@@ -70,8 +89,10 @@ int run(const char **argv, bool mask_stdout) {
 
 done:
   // clean up
-  if (devnull != -1)
-    close(devnull);
+  if (sub_devnull != -1)
+    close(sub_devnull);
+  if (ctrl_devnull != -1)
+    close(ctrl_devnull);
   (void)posix_spawn_file_actions_destroy(&fa);
 
   return rc;
