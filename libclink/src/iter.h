b7f0c161c4cb493aad14004c110d65953b9f6707
Matthew Fernandez <matthew.fernandez@gmail.com>
2021-08-11 21:19:54 -0700

    remove no-longer-used no_lookahead_iter
    Github: related to #30 “remove has_next, lookahead iterators”

--- libclink/src/iter.h
+++ libclink/src/iter.h
@@ -17,25 +17,3 @@ struct clink_iter {
   /// clink_iter_free()
   void (*free)(clink_iter_t *self);
 };
-
-typedef struct no_lookahead_iter no_lookahead_iter_t;
-
-/// an iterator that does not provide has_next()
-struct no_lookahead_iter {
-
-  /// Iterator-specific implementation of clink_iter_next_str(). This should
-  /// only be set for iterators that yield strings.
-  int (*next_str)(no_lookahead_iter_t *self, const char **yielded);
-
-  /// Iterator-specific implementation of clink_iter_next_symbol(). This should
-  /// only be set for iterators that yield symbols.
-  int (*next_symbol)(no_lookahead_iter_t *self, const clink_symbol_t **yielded);
-
-  /// optional state that an iterator may need to track its progress
-  void *state;
-
-  /// optional clean up function for iterator contents that will be called from
-  /// clink_iter_free()
-  void (*free)(no_lookahead_iter_t *self);
-
-};
