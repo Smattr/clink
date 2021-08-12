d5ae9afa77a40cac2f5b4a1679d8700062535264
Matthew Fernandez <matthew.fernandez@gmail.com>
2021-08-11 21:15:19 -0700

    remove no-longer-used iter_symbol_new
    Github: related to #30 “remove has_next, lookahead iterators”

--- libclink/src/iter.h
+++ libclink/src/iter.h
@@ -59,16 +59,3 @@ void no_lookahead_iter_free(no_lookahead_iter_t **it);
  */
 __attribute__((visibility("internal")))
 int iter_str_new(clink_iter_t *it, no_lookahead_iter_t *impl);
-
-/** create a new 1-lookahead symbol iterator from a no-lookahead symbol iterator
- *
- * The iterator being wrapped, `impl`, is assumed to be heap-allocated. Calling
- * `iter_free` on the created 1-lookahead iterator will also cleanup and free
- * the encapsulated no-lookahead iterator.
- *
- * \param it [out] Created 1-lookahead symbol iterator on success
- * \param impl No-lookahead symbol iterator to wrap
- * \returns 0 on success or an errno on failure
- */
-__attribute__((visibility("internal")))
-int iter_symbol_new(clink_iter_t *it, no_lookahead_iter_t *impl);
