3520f0bd7e581e855baa9dfa69bca7f559a465cf
Matthew Fernandez <matthew.fernandez@gmail.com>
2022-04-26 19:32:57 -0700

    vim-cat: add a debug option

--- vim-cat/main.c
+++ vim-cat/main.c
@@ -1,5 +1,7 @@
 #include <clink/clink.h>
 #include <errno.h>
+#include <getopt.h>
+#include <stdbool.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
@@ -20,15 +22,48 @@ static int print(void *ignored, const char *line) {
 
 int main(int argc, char **argv) {
 
-  if (argc != 2) {
-    fprintf(stderr, "usage: %s filename\n", argv[0]);
-    return EXIT_FAILURE;
+  bool debug = false;
+
+  while (true) {
+    static const struct option opts[] = {
+        // clang-format off
+        {"debug", no_argument, 0, 'd'},
+        {"help",  no_argument, 0, 'h'},
+        {0, 0, 0, 0},
+        // clang-format on
+    };
+
+    int index = 0;
+    int c = getopt_long(argc, argv, "dh", opts, &index);
+
+    if (c == -1)
+      break;
+
+    switch (c) {
+
+    case 'd': // --debug
+      debug = true;
+      break;
+
+    case 'h': // --help
+      fprintf(stderr, "usage: %s [--debug] [filename1 [filename2 [...]]]\n",
+              argv[0]);
+      return EXIT_SUCCESS;
+
+    default:
+      return EXIT_FAILURE;
+    }
   }
 
-  int rc = clink_vim_read(argv[1], print, NULL);
-  if (rc != 0) {
-    fprintf(stderr, "failed: %s\n", strerror(rc));
-    return EXIT_FAILURE;
+  if (debug)
+    clink_debug_on();
+
+  for (size_t i = optind; i < (size_t)argc; ++i) {
+    int rc = clink_vim_read(argv[i], print, NULL);
+    if (rc != 0) {
+      fprintf(stderr, "failed: %s\n", strerror(rc));
+      return EXIT_FAILURE;
+    }
   }
 
   return EXIT_SUCCESS;
