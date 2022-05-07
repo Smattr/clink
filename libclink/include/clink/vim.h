f7b5cb1d2b8973e4132db7244aca1af8423df15c
Matthew Fernandez <matthew.fernandez@gmail.com>
2022-05-07 16:17:37 -0700

    remove legacy Vim highlighting API

--- libclink/include/clink/vim.h
+++ libclink/include/clink/vim.h
@@ -1,7 +1,6 @@
 #pragma once
 
 #include <clink/db.h>
-#include <clink/iter.h>
 
 #ifdef __cplusplus
 extern "C" {
@@ -20,27 +19,6 @@ extern "C" {
  */
 CLINK_API int clink_vim_open(const char *filename, unsigned long lineno,
                              unsigned long colno);
-
-/** create a string iterator for the Vim-highlighted lines of the given file
- *
- * \param it [out] Created iterator on success
- * \param filename Source file to read
- * \returns 0 on success, an errno on failure
- */
-CLINK_API int clink_vim_highlight(clink_iter_t **it, const char *filename);
-
-/** Vim-highlight the given file, inserting results into the given database
- *
- * This function is provided as an alternative to `clink_vim_highlight` for when
- * the action being done with every result is simply to insert it into a Clink
- * database. This function avoids the overhead of constructing and managing
- * iterator state.
- *
- * \param db Database to insert into
- * \param filename Source file to read
- * \returns 0 on success or an errno on failure
- */
-CLINK_API int clink_vim_highlight_into(clink_db_t *db, const char *filename);
 
 /** Vim-highlight the given file, returning lines through the callback function
  *
