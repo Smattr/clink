fd7b14adf4d0f0f647a72ff70bc3df2c410ea22a
Matthew Fernandez <matthew.fernandez@gmail.com>
2022-05-12 20:00:09 -0700

    rewrite string queue to use a contiguous buffer
    This is simpler and also more cache friendly. It will also come in handy for
    some upcoming planned concurrency improvements.

--- clink/src/str_queue.c
+++ clink/src/str_queue.c
@@ -5,24 +5,27 @@
 #include <stdlib.h>
 #include <string.h>
 
-/// a node within the linked list that forms the queue itself
-typedef struct node {
-  char *value;
-  struct node *next;
-} node_t;
-
 struct str_queue {
 
   /// strings we have previously added to the queue
   set_t *seen;
 
-  /// head and tail of the queue itself
-  node_t *head;
-  node_t *tail;
+  /// backing memory for the queue
+  char **base;
+  size_t capacity;
 
-  /// number of elements in the queue
+  /// data curently within the backing memory above
+  size_t head;
   size_t size;
 };
+
+static void check_invariant(const str_queue_t *sq) {
+  assert(sq->head <= sq->capacity);
+  assert(sq->size <= sq->capacity);
+  assert(sq->head + sq->size <= sq->capacity);
+
+  (void)sq;
+}
 
 int str_queue_new(str_queue_t **sq) {
 
@@ -42,6 +45,7 @@ done:
   if (rc) {
     str_queue_free(&q);
   } else {
+    check_invariant(q);
     *sq = q;
   }
 
@@ -56,26 +60,35 @@ int str_queue_push(str_queue_t *sq, const char *str) {
   if (str == NULL)
     return EINVAL;
 
+  check_invariant(sq);
+
   // check if we have already seen this string
   int rc = set_add(sq->seen, &str);
   if (rc)
     return rc;
 
-  node_t *n = calloc(1, sizeof(*n));
-  if (n == NULL)
-    return ENOMEM;
+  // do we need to expand the backing memory?
+  if (sq->capacity == sq->head + sq->size) {
+    size_t new_capacity = sq->capacity * 2;
+    if (new_capacity == 0)
+      new_capacity = 4096 / sizeof(sq->base[0]);
 
-  n->value = strdup(str);
-  if (n->value == NULL) {
-    free(n);
-    return ENOMEM;
+    char **b = realloc(sq->base, new_capacity * sizeof(sq->base[0]));
+    if (b == NULL)
+      return ENOMEM;
+
+    sq->base = b;
+    sq->capacity = new_capacity;
+
+    check_invariant(sq);
   }
 
-  if (sq->tail == NULL) {
-    sq->head = sq->tail = n;
-  } else {
-    sq->tail->next = n;
-    sq->tail = n;
+  {
+    size_t tail = sq->head + sq->size;
+    assert(tail < sq->capacity);
+    sq->base[tail] = strdup(str);
+    if (sq->base[tail] == NULL)
+      return ENOMEM;
   }
 
   ++sq->size;
@@ -96,21 +109,16 @@ int str_queue_pop(str_queue_t *sq, char **str) {
   if (str == NULL)
     return EINVAL;
 
+  check_invariant(sq);
+
   // is the queue empty?
-  if (sq->head == NULL)
+  if (str_queue_size(sq) == 0)
     return ENOMSG;
 
-  *str = sq->head->value;
+  *str = sq->base[sq->head];
 
   // remove the head
-  node_t *head = sq->head;
-  sq->head = sq->head->next;
-  free(head);
-
-  // if the queue only had one element, we need to blank the tail too
-  if (head == sq->tail)
-    sq->tail = NULL;
-
+  ++sq->head;
   --sq->size;
 
   return 0;
@@ -121,14 +129,9 @@ void str_queue_free(str_queue_t **sq) {
   if (sq == NULL || *sq == NULL)
     return;
 
-  for (node_t *n = (*sq)->head; n != NULL;) {
-    node_t *next = n->next;
-    free(n->value);
-    free(n);
-    n = next;
-  }
-  (*sq)->head = NULL;
-  (*sq)->tail = NULL;
+  for (size_t i = 0; i < str_queue_size(*sq); ++i)
+    free((*sq)->base[(*sq)->head + i]);
+  free((*sq)->base);
 
   set_free(&(*sq)->seen);
 
