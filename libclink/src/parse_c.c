855fa370a582aada87f2f1a56f5cbe74501a1428
Matthew Fernandez <matthew.fernandez@gmail.com>
2022-07-15 17:35:26 -0700

    specialise C parser
    This narrows what the parser recognises to the subset of things valid in C but
    not in C++. Unfortunately we cannot immediately do the same to the C++ parser
    because it is used for parsing .h files that may be C, so we need to retain some
    more permissiveness.

--- libclink/src/parse_c.c
+++ libclink/src/parse_c.c
@@ -21,14 +21,13 @@ int clink_parse_c(clink_db_t *db, const char *filename, size_t argc,
     return errno;
 
   static const char *KEYWORDS[] = {
-    // just enable everything
 #define C99(keyword) #keyword,
 #define C11(keyword) #keyword,
 #define C23(keyword) #keyword,
 #define CXX_C(keyword) #keyword,
-#define CXX(keyword) #keyword,
-#define CXX_11(keyword) #keyword,
-#define CXX_20(keyword) #keyword,
+#define CXX(keyword)
+#define CXX_11(keyword)
+#define CXX_20(keyword)
 #include "c_keywords.inc"
 #undef C99
 #undef C11
@@ -41,10 +40,9 @@ int clink_parse_c(clink_db_t *db, const char *filename, size_t argc,
   static const size_t KEYWORDS_LENGTH = sizeof(KEYWORDS) / sizeof(KEYWORDS[0]);
 
   static const char *DEFN_LEADERS[] = {
-      "auto",     "bool",     "char",    "char8_t", "char16_t",
-      "char32_t", "class",    "double",  "enum",    "float",
-      "int",      "long",     "short",   "signed",  "struct",
-      "union",    "unsigned", "wchar_t", "void",    "_Bool",
+      "auto",   "bool",  "char",     "double", "enum",
+      "float",  "int",   "long",     "short",  "signed",
+      "struct", "union", "unsigned", "void",   "_Bool",
   };
   static const size_t DEFN_LEADERS_LENGTH =
       sizeof(DEFN_LEADERS) / sizeof(DEFN_LEADERS[0]);
