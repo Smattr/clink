0035e3e0f727a905ccb5e415c5f0d0a386cd7228
Matthew Fernandez <matthew.fernandez@gmail.com>
2022-07-17 08:37:28 -0700

    abstract comparison for spans

--- libclink/src/span.h
+++ libclink/src/span.h
@@ -1,9 +1,26 @@
 #pragma once
 
+#include <assert.h>
+#include <stdbool.h>
 #include <stddef.h>
+#include <string.h>
 
 /// a non-owning string reference
 typedef struct {
   const char *base; ///< start of the range
   size_t size;      ///< number of bytes in the range
 } span_t;
+
+static inline bool span_eq(span_t a, const char *b) {
+
+  assert(a.base != NULL);
+  assert(b != NULL);
+
+  if (a.size != strlen(b))
+    return false;
+
+  if (strncmp(a.base, b, a.size) != 0)
+    return false;
+
+  return true;
+}
