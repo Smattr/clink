a877a22650908add239fd1c9520ee02b916303bb
Matthew Fernandez <matthew.fernandez@gmail.com>
2022-07-20 17:24:05 -0700

    adjust test case to not recognise calls within macros
    It was inaccurate to expect this because `a_call` here is not actually known to
    be a function call. It could be anything and we do not know until the macro is
    expanded. So we should expect it to be vague here.

--- test/cases/macro-definition-parent.c
+++ test/cases/macro-definition-parent.c
@@ -15,7 +15,7 @@
 // CHECK: ref|{%s}|2|4|13|FOO
 
 // RUN: echo 'select * from symbols where name = "a_call";' | sqlite3 {%t}
-// CHECK: a_call|{%s}|1|6|13|BAR
+// CHECK: a_call|{%s}|2|6|13|BAR
 
 // RUN: echo 'select * from symbols where name = "our_ref";' | sqlite3 {%t}
 // CHECK: our_ref|{%s}|2|9|35|BAZ
