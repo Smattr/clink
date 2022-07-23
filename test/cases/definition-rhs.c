f5beab4b06a1e9484e0be45b3177593007f6451d
Matthew Fernandez <matthew.fernandez@gmail.com>
2022-07-23 13:37:59 -0700

    add a test case for incorrect multiplication parsing

---
+++ test/cases/definition-rhs.c
@@ -0,0 +1,11 @@
+/// check if multiplication is incorrectly parsed as pointer definition
+
+void foo(void) {
+  int x = y * z;
+}
+
+// XFAIL: True
+
+// RUN: clink --build-only --database {tmp} --debug {__file__} >/dev/null
+// RUN: echo 'select * from symbols where name = "z";' | sqlite3 {tmp}
+// CHECK: z|{__file__}|2|4|15|foo
