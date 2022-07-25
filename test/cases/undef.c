a002b9db10a26c4b7bbf310380d176c5e89cd702
Matthew Fernandez <matthew.fernandez@gmail.com>
2022-07-24 17:27:30 -0700

    add a test case demonstrating a #undef bug

---
+++ test/cases/undef.c
@@ -0,0 +1,8 @@
+/// undefining something should not be counted as a definition
+
+#undef FOO
+
+// XFAIL: True
+// RUN: clink --build-only --database {tmp} --debug {__file__} >/dev/null
+// RUN: echo 'select * from symbols;' | sqlite3 {tmp}
+// CHECK: FOO|{__file__}|2|3|8|
