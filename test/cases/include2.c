06e93a9c8be62bbc6c5ba2a9bbfaa90c689b093e
Matthew Fernandez <matthew.fernandez@gmail.com>
2022-07-24 17:09:44 -0700

    flip run line ordering on some test cases to ease removing XFAIL

--- test/cases/include2.c
+++ test/cases/include2.c
@@ -1,8 +1,8 @@
 /// check we can recognise a basic include
 
+#include <foo.h>
+
 // XFAIL: True
 // RUN: clink --build-only --database {tmp} {__file__} >/dev/null
 // RUN: echo 'select * from symbols;' | sqlite3 {tmp}
-// CHECK: foo.h|{__file__}|3|8|1|
-
-#include <foo.h>
+// CHECK: foo.h|{__file__}|3|3|1|
