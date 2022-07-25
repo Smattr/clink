6e3b43f780c3bfa33aa21f130e7cb347f54d5d73
Matthew Fernandez <matthew.fernandez@gmail.com>
2022-07-24 17:16:48 -0700

    implement support for recognising #includes in C

--- test/cases/include2.c
+++ test/cases/include2.c
@@ -2,7 +2,6 @@
 
 #include <foo.h>
 
-// XFAIL: True
 // RUN: clink --build-only --database {tmp} --debug {__file__} >/dev/null
 // RUN: echo 'select * from symbols;' | sqlite3 {tmp}
 // CHECK: foo.h|{__file__}|3|3|11|
