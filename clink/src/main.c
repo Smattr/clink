edbfb937abb0600c8c0514c396b9d4c849ab3312
Matthew Fernandez <matthew.fernandez@gmail.com>
2022-07-28 07:39:47 -0700

    fix: remove '-I' flag recognition
    This option was removed in 9b2b822dd91edc4b917de98a6814e6f29f894603.

--- clink/src/main.c
+++ clink/src/main.c
@@ -62,7 +62,7 @@ static void parse_args(int argc, char **argv) {
     };
 
     int index = 0;
-    int c = getopt_long(argc, argv, "bdf:hI:j:ls:", opts, &index);
+    int c = getopt_long(argc, argv, "bdf:hj:ls:", opts, &index);
 
     if (c == -1)
       break;
