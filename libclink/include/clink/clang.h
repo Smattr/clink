a6b01b139aaf7bd8e0ad082ca613af733f9db3e8
Matthew Fernandez <matthew.fernandez@gmail.com>
2022-07-31 15:26:14 -0700

    resurrect a version of the libclang-based parser
    This is more or less as it was when we removed it. The current plan is to refine
    this to:
    
      1. Make it understand macro expansions the way `libclang-ls` now does.
      2. Add functionality for comprehending compilation databases, that allow
         parsing a file fully accurately.

---
+++ libclink/include/clink/clang.h
@@ -0,0 +1,23 @@
+#pragma once
+
+#include <clink/db.h>
+#include <stddef.h>
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+/** parse the given C/C++ file with Clang
+ *
+ * \param db Database to insert into
+ * \param filename Path to source file to parse
+ * \param argc Number of Clang command line arguments
+ * \param argv Clang commang line arguments
+ * \returns 0 on success or an errno on failure
+ */
+CLINK_API int clink_parse_with_clang(clink_db_t *db, const char *filename,
+                                     size_t argc, const char **argv);
+
+#ifdef __cplusplus
+}
+#endif
