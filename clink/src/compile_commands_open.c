7f8e6468cfddc3418b615c00fa7ee6acdf55d178
Matthew Fernandez <matthew.fernandez@gmail.com>
2022-08-02 20:28:21 -0700

    do an 'access' check before asking libclang to parse compile_commands.json
    It seems libclang behaves a bit unpleasantly and outputs errors on stderr and
    whatnot when asked to open a missing compile_commands.json.

--- clink/src/compile_commands_open.c
+++ clink/src/compile_commands_open.c
@@ -1,11 +1,13 @@
 #include "../../common/compiler.h"
 #include "compile_commands.h"
+#include "path.h"
 #include <assert.h>
 #include <clang-c/CXCompilationDatabase.h>
 #include <errno.h>
 #include <pthread.h>
-#include <stddef.h>
+#include <stdlib.h>
 #include <string.h>
+#include <unistd.h>
 
 int compile_commands_open(compile_commands_t *cc, const char *directory) {
 
@@ -13,6 +15,18 @@ int compile_commands_open(compile_commands_t *cc, const char *directory) {
   assert(directory != NULL);
 
   memset(cc, 0, sizeof(*cc));
+
+  // check whether compile_commands.json exists
+  {
+    char *compile_commands = NULL;
+    int rc = join(directory, "compile_commands.json", &compile_commands);
+    if (rc != 0)
+      return rc;
+    rc = access(compile_commands, R_OK);
+    free(compile_commands);
+    if (rc != 0)
+      return rc;
+  }
 
   CXCompilationDatabase_Error err;
   cc->db = clang_CompilationDatabase_fromDirectory(directory, &err);
