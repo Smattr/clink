5691faf0fd80957aae52eca74b8670ba260182c0
Matthew Fernandez <matthew.fernandez@gmail.com>
2022-08-20 21:34:09 -0700

    add test case demonstrating out of bounds read in C preprocessor

---
+++ test/cases/ifdef-no-arg.c
@@ -0,0 +1,7 @@
+/// check we do not read out of bounds if a preprocessor directive runs right up
+/// against the end of the file
+
+// XFAIL: True
+// RUN: clink --build-only --database={%t} --debug {%s} >/dev/null
+
+#ifdef
