8fbbafb9471dfd82ed49eea75c838c58fd015eca
Matthew Fernandez <matthew.fernandez@gmail.com>
2022-08-28 19:28:56 -0700

    port from Ncurses to something internal
    The motivation for this was to get simpler colour rendering and a simpler path
    to UTF-8 support. But it turns out this dramatically simplifies the code, fixes
    some visual artifacts, and increases responsiveness.
    
    Note that the strategy taken by the internal replacement is a little odd in that
    we disable echo and never re-enable it. It is simpler to mimic the effect of
    echoing by our own manipulation of the screen, ensuring we are always in full
    control of the content.

--- clink/src/colour.h
+++ clink/src/colour.h
@@ -1,20 +1,9 @@
-// Support for converting between different formats for describing the colour
-// and other attributes of terminal text.
-//
-// There are two different formats dealt with below:
-//
-//   1. ANSI terminal codes (e.g. "ESC[32;1m")
-//   2. Ncurses colour pairs
+/// \file
+/// \brief support for converting coloured text to black-and-white text
 
 #pragma once
 
 #include <stdio.h>
-
-/** configure Ncurses colour pairs for use later
- *
- * \return 0 on success, non-zero on failure
- */
-int init_ncurses_colours(void);
 
 /** output a string, stripping ANSI codes
  *
@@ -23,17 +12,3 @@ int init_ncurses_colours(void);
  * \return 0 on success
  */
 int printf_bw(const char *s, FILE *stream);
-
-/** output a string using Ncurses, stripping ANSI codes
- *
- * \param s String to output
- * \return 0 on success
- */
-void printw_bw(const char *s);
-
-/** output a string using Ncurses, translating ANSI codes to Ncurses colours
- *
- * \param s String to output
- * \return 0 on success
- */
-void printw_colour(const char *s);
