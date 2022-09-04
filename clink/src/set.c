a82bd2bfba914cc5d18754bad7d295f006510d0a
Matthew Fernandez <matthew.fernandez@gmail.com>
2022-09-03 17:38:38 -0700

    remove clang-format disabling in set code
    For reasons I have not bothered diving into, `clang-format` is not stable on
    this code. Running and re-running it would move the indentation of the
    “// clang-format off” line backwards and forwards. Rather than continuing
    dealing with this annoyance, lets just drop the disabling.

--- clink/src/set.c
+++ clink/src/set.c
@@ -60,16 +60,21 @@ static uint64_t hash(const char *key) {
   const unsigned char *data2 = data;
 
   switch (len & 7) {
-    // clang-format off
-    case 7: h ^= (uint64_t)data2[6] << 48; // fall through
-    case 6: h ^= (uint64_t)data2[5] << 40; // fall through
-    case 5: h ^= (uint64_t)data2[4] << 32; // fall through
-    case 4: h ^= (uint64_t)data2[3] << 24; // fall through
-    case 3: h ^= (uint64_t)data2[2] << 16; // fall through
-    case 2: h ^= (uint64_t)data2[1] << 8;  // fall through 
-    case 1: h ^= (uint64_t)data2[0];
+  case 7:
+    h ^= (uint64_t)data2[6] << 48; // fall through
+  case 6:
+    h ^= (uint64_t)data2[5] << 40; // fall through
+  case 5:
+    h ^= (uint64_t)data2[4] << 32; // fall through
+  case 4:
+    h ^= (uint64_t)data2[3] << 24; // fall through
+  case 3:
+    h ^= (uint64_t)data2[2] << 16; // fall through
+  case 2:
+    h ^= (uint64_t)data2[1] << 8; // fall through
+  case 1:
+    h ^= (uint64_t)data2[0];
     h *= m;
-    // clang-format on
   }
 
   h ^= h >> r;
