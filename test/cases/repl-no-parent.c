3078e187140787b657b3635e7099482b66763b57
Matthew Fernandez <matthew.fernandez@gmail.com>
2022-09-14 21:10:25 -0700

    add an example of Cscope infidelity
    This is an example that currently causes problems when interacting with Vim.

---
+++ test/cases/repl-no-parent.c
@@ -0,0 +1,13 @@
+/// symbols without parents should be listed with themselves as a parent in the
+/// REPL
+/// (no, this does not make any sense, but it is what Cscope does)
+
+void foo() {
+}
+
+// XFAIL: True
+
+// RUN: clink --build-only --database={%t} --debug {%s} >/dev/null
+// RUN: echo '0foo' | clink-repl -dl -f {%t}
+// CHECK: >> cscope: 1 lines
+// CHECK: {%s} foo 5 void foo() {{
