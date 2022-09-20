abdfc356a30b59d3e1a6bc5b3acc6eb2848e371f
Matthew Fernandez <matthew.fernandez@gmail.com>
2022-09-19 19:07:14 -0700

    add a test case for left trimming database content
    Github: #133 “ltrim during database construction”

---
+++ test/cases/ltrim.c
@@ -0,0 +1,22 @@
+/// check that database results are left-trimmed before insertion
+
+void foo(void) {
+  int x = 0;
+}
+
+// XFAIL: True
+
+// parse this into a database with full content extraction
+// RUN: clink --build-only --database {%t} --debug --syntax-highlight=eager {%s} >/dev/null
+
+// we should have content extracted
+// RUN: echo 'select COUNT(*) from content where line = 4;' | sqlite3 {%t}
+// CHECK: 1
+
+// it should have been stripped for leading space
+//   1. ask for the highlighted version of line 4 from this file
+//   2. use `sed` to strip ANSI colour sequences
+//   3. use `sed` to add a marker to suppress integration.py’s own left-trimming
+//      behaviour
+// RUN: echo 'select body from content where line = 4;' | sqlite3 {%t} | sed -E 's/\x1b\[[0-9;]+m//g' | sed -E 's/(.*)/start\1/'
+// CHECK: startint x = 0;
