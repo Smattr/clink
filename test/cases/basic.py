946d608bb1a3b11286fa1de0e4bee8a183117a0c
Matthew Fernandez <matthew.fernandez@gmail.com>
2022-09-24 09:41:31 -0700

    recognise Python comments in test cases instead of hard coding '//'

--- test/cases/basic.py
+++ test/cases/basic.py
@@ -1,4 +1,4 @@
-// basic Python test
+# basic Python test
 
 def foo(x: int):
   return x + 1
@@ -7,14 +7,14 @@ class Bar():
   def __init__():
     pass
 
-// RUN: clink --build-only --database {%t} --debug {%s} >/dev/null
+# RUN: clink --build-only --database {%t} --debug {%s} >/dev/null
 
-// RUN: echo 'select path, category, line, col from symbols where name = "foo" and line < 12;' | sqlite3 {%t}
-// CHECK: {%s}|0|3|5
+# RUN: echo 'select path, category, line, col from symbols where name = "foo" and line < 12;' | sqlite3 {%t}
+# CHECK: {%s}|0|3|5
 
-// RUN: echo 'select path, line, col from symbols where name = "x" and line < 12 order by line;' | sqlite3 {%t}
-// CHECK: {%s}|3|9
-// CHECK: {%s}|4|10
+# RUN: echo 'select path, line, col from symbols where name = "x" and line < 12 order by line;' | sqlite3 {%t}
+# CHECK: {%s}|3|9
+# CHECK: {%s}|4|10
 
-// RUN: echo 'select * from symbols where name = "Bar" and line < 12;' | sqlite3 {%t}
-// CHECK: Bar|{%s}|0|6|7
+# RUN: echo 'select * from symbols where name = "Bar" and line < 12;' | sqlite3 {%t}
+# CHECK: Bar|{%s}|0|6|7
