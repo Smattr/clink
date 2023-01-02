d29ec6089debd15a1c980827e992a9ec7bd4ade5
Matthew Fernandez <matthew.fernandez@gmail.com>
2023-01-02 14:49:07 -0800

    use head and tail indices instead of pointers in the string queue
    This is a significant simplification that allows us to (1) not adjust head and
    tail when reallocating the underlying storage and (2) drop guards/workarounds
    for performing UB addition on null pointers when capacity is 0.
    
    Note that no modulo arithmetic is required because of the two-phase assumption
    of the queue. That is, because head and tail advances are never interleaved, the
    ordering between head and tail never inverts.

--- clink/src/str_queue.c
+++ clink/src/str_queue.c
@@ -16,23 +16,19 @@ struct str_queue {
   size_t capacity;
 
   /// data curently within the backing memory above
-  const char **head;
-  const char **tail;
+  size_t head;
+  size_t tail;
 };
 
 static void check_invariant(const str_queue_t *sq) {
 
-  const char **head = __atomic_load_n(&sq->head, __ATOMIC_ACQUIRE);
+  size_t head = __atomic_load_n(&sq->head, __ATOMIC_ACQUIRE);
 
   // head should be within the allocated region
-  assert(head >= sq->base);
-  if (sq->capacity != 0)
-    assert(head <= sq->base + sq->capacity);
+  assert(head <= sq->capacity);
 
   // tail should be within the allocated region
-  assert(sq->tail >= sq->base);
-  if (sq->capacity != 0)
-    assert(sq->tail <= sq->base + sq->capacity);
+  assert(sq->tail <= sq->capacity);
 
   // head should precede tail
   assert(head <= sq->tail);
@@ -76,7 +72,7 @@ int str_queue_push(str_queue_t *sq, const char *str) {
 
   check_invariant(sq);
 
-  assert(sq->base == sq->head &&
+  assert(sq->head == 0 &&
          "head and base are out of sync; pop in str_queue phase 1?");
 
   // check if we have already seen this string
@@ -85,7 +81,7 @@ int str_queue_push(str_queue_t *sq, const char *str) {
     return rc;
 
   // do we need to expand the backing memory?
-  if (sq->capacity == 0 || sq->tail == sq->base + sq->capacity) {
+  if (sq->capacity == sq->tail) {
     size_t new_capacity = sq->capacity * 2;
     if (new_capacity == 0)
       new_capacity = 4096 / sizeof(sq->base[0]);
@@ -95,15 +91,13 @@ int str_queue_push(str_queue_t *sq, const char *str) {
       return ENOMEM;
 
     sq->base = b;
-    sq->tail = sq->base + (sq->tail - sq->head);
-    sq->head = sq->base;
     sq->capacity = new_capacity;
 
     check_invariant(sq);
   }
 
-  assert(sq->tail < sq->base + sq->capacity);
-  *sq->tail = str;
+  assert(sq->tail < sq->capacity);
+  sq->base[sq->tail] = str;
   ++sq->tail;
 
   return 0;
@@ -126,15 +120,15 @@ int str_queue_pop(str_queue_t *sq, const char **str) {
 
   // we can read tail unsynchronised because it is not modified since the last
   // multithreading sequence point, but we need to protect our read of head
-  const char **head = __atomic_load_n(&sq->head, __ATOMIC_ACQUIRE);
-  const char **tail = sq->tail;
+  size_t head = __atomic_load_n(&sq->head, __ATOMIC_ACQUIRE);
+  size_t tail = sq->tail;
 
   do {
     // is the queue empty?
     if (head == tail)
       return ENOMSG;
 
-    *str = *head;
+    *str = sq->base[head];
 
     // try to remove the head
   } while (!__atomic_compare_exchange_n(&sq->head, &head, head + 1, false,
