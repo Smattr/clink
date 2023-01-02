1a41f8b35f19be52aa19bdb2696aab4c5ccf7ef5
Matthew Fernandez <matthew.fernandez@gmail.com>
2023-01-02 15:08:58 -0800

    add a test case for atomic built-ins
    Github: #179 “parsing C in Clang mode does not detect __atomic built-ins”

---
+++ test/cases/atomic-builtins.c
@@ -0,0 +1,13 @@
+/// can we recognise calls to the GCC atomic built-ins?
+
+int foo(void) {
+  int i = 0;
+  return __atomic_load_n(&i, __ATOMIC_ACQUIRE);
+}
+
+// XFAIL: True
+// RUN: clink --build-only --database={%t} --debug --parse-c=clang {%s} >/dev/null
+// RUN: echo 'select name, path, line, col from symbols where name = "__ATOMIC_ACQUIRE";' | sqlite3 {%t}
+// CHECK: __ATOMIC_ACQUIRE|{%s}|5|30
+// RUN: echo 'select name, path, category, line, col from symbols where name = "__atomic_load_n";' | sqlite3 {%t}
+// CHECK: __atomic_load_n|{%s}|1|5|10
