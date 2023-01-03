4f1b1b0b420e354ee220fcd16dfdc260fe798f9d
Matthew Fernandez <matthew.fernandez@gmail.com>
2023-01-02 16:04:02 -0800

    add a variation on the atomic built-ins test
    There seems to be a pattern here, in that all the built-ins _except_ the __sync
    and __atomic ones are recognised. I am unsure why.
    
    Github: #179 “parsing C in Clang mode does not detect __atomic built-ins”

---
+++ test/cases/atomic-builtins-2.c
@@ -0,0 +1,11 @@
+/// can we recognise calls to the GCC atomic built-ins?
+
+int foo(void) {
+  int i = 0;
+  return __sync_fetch_and_add(&i, 1);
+}
+
+// XFAIL: True
+// RUN: clink --build-only --database={%t} --debug --parse-c=clang {%s} >/dev/null
+// RUN: echo 'select name, path, category, line, col from symbols where name = "__sync_fetch_and_add";' | sqlite3 {%t}
+// CHECK: __sync_fetch_and_add|{%s}|1|5|10
