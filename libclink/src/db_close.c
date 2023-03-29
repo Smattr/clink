9594a164feb2ac59ea4a89cb995926d1393fee2f
Matthew Fernandez <matthew.fernandez@gmail.com>
2023-03-29 13:57:04 -0700

    introduce a per-db connection mutex

--- libclink/src/db_close.c
+++ libclink/src/db_close.c
@@ -1,7 +1,9 @@
 #include "db.h"
 #include "re.h"
 #include <clink/db.h>
+#include <pthread.h>
 #include <sqlite3.h>
+#include <stdbool.h>
 #include <stdlib.h>
 
 void clink_db_close(clink_db_t **db) {
@@ -9,6 +11,10 @@ void clink_db_close(clink_db_t **db) {
   // allow freeing NULL
   if (db == NULL || *db == NULL)
     return;
+
+  if ((*db)->bulk_operation_available)
+    (void)pthread_mutex_destroy(&(*db)->bulk_operation);
+  (*db)->bulk_operation_available = false;
 
   re_free(&(*db)->regexes);
 
