5830426468142e48f19674f671fc7f6b01b31d08
Matthew Fernandez <matthew.fernandez@gmail.com>
2023-04-15 21:03:11 -0700

    add a test for the REPL’s lack of context
    Github: #206 “lack of context in clink-repl”

---
+++ test/cases/repl-context.c
@@ -0,0 +1,13 @@
+/// can the REPL generate context on demand?
+
+int x;
+
+// parse this file and generate a database
+// RUN: clink --build-only --database={%t} --debug --parse-c=clang --syntax-highlighting=lazy {%s} >/dev/null
+
+// At this point we will have no context for this file because it was not syntax
+// highlighted. Ask the REPL to give it us anyway.
+// RUN: echo "1x" | clink-repl -f {%t}
+// CHECK: >> cscope: 1 lines
+// XFAIL: True
+// CHECK: {%s} x 3 int x;
