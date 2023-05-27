2547221674dbdfc87e97438783e1826a8ba10bdb
Matthew Fernandez <matthew.fernandez@gmail.com>
2023-05-27 15:07:54 -0700

    [nfc] outline the core of 'clink_db_add_line'

--- libclink/src/db_add_line.c
+++ libclink/src/db_add_line.c
@@ -2,10 +2,55 @@
 #include "debug.h"
 #include "get_id.h"
 #include "sql.h"
+#include <assert.h>
 #include <clink/db.h>
 #include <errno.h>
 #include <sqlite3.h>
 #include <stddef.h>
+
+static int add_line(clink_db_t *db, clink_record_id_t path,
+                    unsigned long lineno, const char *line) {
+
+  assert(db != NULL);
+  assert(path != -1);
+  assert(lineno != 0);
+  assert(line != NULL);
+
+  int rc = 0;
+  sqlite3_stmt *s = NULL;
+
+  // insert into the content table
+
+  static const char CONTENT_INSERT[] =
+      "insert or replace into content (path, "
+      "line, body) values (@path, @line, @body);";
+
+  if (ERROR((rc = sql_prepare(db->db, CONTENT_INSERT, &s))))
+    goto done;
+
+  if (ERROR((rc = sql_bind_int(s, 1, path))))
+    goto done;
+
+  if (ERROR((rc = sql_bind_int(s, 2, lineno))))
+    goto done;
+
+  if (ERROR((rc = sql_bind_text(s, 3, line))))
+    goto done;
+
+  {
+    int r = sqlite3_step(s);
+    if (ERROR(r != SQLITE_DONE)) {
+      rc = sql_err_to_errno(r);
+      goto done;
+    }
+  }
+
+done:
+  if (s != NULL)
+    sqlite3_finalize(s);
+
+  return rc;
+}
 
 int clink_db_add_line(clink_db_t *db, const char *path, unsigned long lineno,
                       const char *line) {
@@ -29,42 +74,14 @@ int clink_db_add_line(clink_db_t *db, const char *path, unsigned long lineno,
     return EINVAL;
 
   int rc = 0;
-  sqlite3_stmt *s = NULL;
 
   // find the identifier for this line’s path
   clink_record_id_t id = -1;
   if (ERROR((rc = get_id(db, path, &id))))
     goto done;
 
-  // insert into the content table
-
-  static const char CONTENT_INSERT[] =
-      "insert or replace into content (path, "
-      "line, body) values (@path, @line, @body);";
-
-  if (ERROR((rc = sql_prepare(db->db, CONTENT_INSERT, &s))))
-    goto done;
-
-  if (ERROR((rc = sql_bind_int(s, 1, id))))
-    goto done;
-
-  if (ERROR((rc = sql_bind_int(s, 2, lineno))))
-    goto done;
-
-  if (ERROR((rc = sql_bind_text(s, 3, line))))
-    goto done;
-
-  {
-    int r = sqlite3_step(s);
-    if (ERROR(r != SQLITE_DONE)) {
-      rc = sql_err_to_errno(r);
-      goto done;
-    }
-  }
+  rc = add_line(db, id, lineno, line);
 
 done:
-  if (s != NULL)
-    sqlite3_finalize(s);
-
   return rc;
 }
