Smattr/clink

commit 319369a9c7c650bc66877c7d9710876bf74fccfe

thread originating identifier through when parsing with Cscope
This is a minor optimisation (its effect is in the noise), but seems worth
avoiding the overhead of an extra database call.

clink/src/build.c
@@ -102,7 +102,10 @@ static const char *filetype(const char *path) {
   return is_c(path) ? "C" : "C++";
 }
 
-static int parse(unsigned long thread_id, clink_db_t *db, const char *path) {
+static int parse(unsigned long thread_id, clink_db_t *db, const char *path,
+                 clink_record_id_t id) {
+
+  assert(id >= 0);
 
   int rc = 0;
 
@@ -138,7 +141,7 @@ static int parse(unsigned long thread_id, clink_db_t *db, const char *path) {
   } else if (use_cscope(path)) {
     progress_status(thread_id, "Cscope-parsing %s file %s", filetype(path),
                     display);
-    rc = clink_parse_with_cscope(db, path, -1);
+    rc = clink_parse_with_cscope(db, path, id);
 
   } else if (is_asm(path)) {
 
@@ -253,9 +256,10 @@ static int process(unsigned long thread_id, pthread_t *threads, clink_db_t *db,
     clink_db_remove(db, path);
 
     // insert a new record for the file
-    (void)clink_db_add_record(db, path, hash, timestamp, NULL);
+    clink_record_id_t id = -1;
+    (void)clink_db_add_record(db, path, hash, timestamp, &id);
 
-    if (UNLIKELY((rc = parse(thread_id, db, path))))
+    if (UNLIKELY((rc = parse(thread_id, db, path, id))))
       break;
 
     // bump the progress counter

commit 23cec2bf6fd8f86c4afa92d2923567c417695019

fix: do not ignore record insertion failure during DB construction
319369a9c7c650bc66877c7d9710876bf74fccfe introduced a variable (`id`) to the
record addition that was then threaded through later functions without adjusting
the `clink_db_add_record` call to account for the fact that using this when the
call has failed is invalid.

Github: fixes #242 “assertion id >= 0 failed”
Reported-by: Timothy Madden

clink/src/build.c
@@ -282,7 +282,8 @@ static int process(unsigned long thread_id, pthread_t *threads, clink_db_t *db,
 
     // insert a new record for the file
     clink_record_id_t id = -1;
-    (void)clink_db_add_record(db, path, hash, timestamp, &id);
+    if (UNLIKELY((rc = clink_db_add_record(db, path, hash, timestamp, &id))))
+      break;
 
     if (UNLIKELY((rc = parse(thread_id, db, path, id))))
       break;