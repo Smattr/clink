e42016dd528819794d6c7963ea764ae1d83fbbbb
Matthew Fernandez <matthew.fernandez@gmail.com>
2024-11-28 17:08:38 +1100

    add test case for 'enum class' bug
    This was stumbled across while using GCC 12.4.0 and encountering the file
    gcc/testsuite/g++.dg/cpp0x/enum26.C.

---
+++ test/cases/enum-class.c
@@ -0,0 +1,14 @@
+/// does `enum class` trip up Cscope-based parsing?
+///
+/// Cscope treats `class` as a leader and expects a name following it. But
+/// `class` is not a keyword in C, as opposed to C++. Because Cscope is agnostic
+/// as to whether it is parsing C or C++, it emits two global definitions:
+///   1. `class`, corresponding to the C semantics
+///   2. An anonymous one (named ``)
+/// It is unclear to me what the purpose of the second one is, because nothing
+/// can reference this, but nevertheless it is written to Cscope’s database.
+
+enum class { };
+
+// XFAIL: True
+// RUN: clink --build-only --database={%t} --debug --parse-c=cscope {%s} >/dev/null
