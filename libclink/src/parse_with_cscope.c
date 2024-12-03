Smattr/clink

commit 12e8e3113855a98cf9dd32bd2c6ca4688ca2f4fa

use a sliding 100-symbol window when inserting symbols in Cscope parser
This significantly accelerates symbol insertion by chunking when the database is
operated on. We essentially reduce the amount of database contention by doing
this. On a recent Graphviz checkout on an 8-core machine, this drops database
creation time from 9.95s to 8.66s.

libclink/src/parse_with_cscope.c
@@ -163,6 +163,11 @@ static int parse_into(clink_db_t *db, const char *cscope_out,
   // function/macro/struct we are within
   span_t parent = {0};
 
+  // symbols queued to be inserted
+  enum { SYMBOL_WINDOW = 100 };
+  symbol_t pending[SYMBOL_WINDOW];
+  size_t pending_size = 0;
+
   while (s.offset < s.size) {
 
     clink_category_t category = CLINK_REFERENCE;
@@ -250,8 +255,15 @@ static int parse_into(clink_db_t *db, const char *cscope_out,
                       .name = symbol,
                       .path = filename,
                       .parent = parent};
-      if (ERROR((rc = add_symbol(db, sym))))
-        goto done;
+      if (pending_size == sizeof(pending) / sizeof(pending[0])) {
+        // flush the pending symbols
+        if (ERROR((rc = add_symbols(db, pending_size, pending))))
+          goto done;
+        pending_size = 0;
+      }
+      // enqueue the current symbol
+      pending[pending_size] = sym;
+      ++pending_size;
     }
 
     if (can_be_parent)
@@ -262,6 +274,10 @@ static int parse_into(clink_db_t *db, const char *cscope_out,
   }
 
 done:
+  // flush any remaining pending symbols
+  if (rc == 0 && pending_size > 0)
+    rc = add_symbols(db, pending_size, pending);
+
   mmap_close(f);
 
   return rc;

commit da90fb79e79288af8d9c095203f44547bf67aa29

fix use-before-initialisation during Cscope parsing
12e8e3113855a98cf9dd32bd2c6ca4688ca2f4fa introduced a bug in this code wherein
early failures would jump past the initialisation of `pending` and
`pending_size` to code that then used these variables.

Frustratingly there seems to be no GCC or Clang warning to catch this (IMHO,
clear) mistake. -Wjump-misses-init notices it, but has so many false positives
it is not practical to turn on by default.

libclink/src/parse_with_cscope.c
@@ -138,6 +138,11 @@ static int parse_into(clink_db_t *db, const char *cscope_out,
   int rc = 0;
   mmap_t f = {0};
 
+  // symbols queued to be inserted
+  enum { SYMBOL_WINDOW = 1000 };
+  symbol_t pending[SYMBOL_WINDOW];
+  size_t pending_size = 0;
+
   // if the caller did not give us an identifier, look it up now
   if (id < 0) {
     if (ERROR((rc = get_id(db, filename, &id))))
@@ -161,11 +166,6 @@ static int parse_into(clink_db_t *db, const char *cscope_out,
 
   // function/macro/struct we are within
   span_t parent = {0};
-
-  // symbols queued to be inserted
-  enum { SYMBOL_WINDOW = 1000 };
-  symbol_t pending[SYMBOL_WINDOW];
-  size_t pending_size = 0;
 
   while (s.offset < s.size) {
 