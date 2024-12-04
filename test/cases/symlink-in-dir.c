1f5077feaa7cbc8e991dd8039fcef52a6c4c010c
Matthew Fernandez <matthew.fernandez@gmail.com>
2024-12-04 17:39:33 +1100

    fix erroring out when Cscope-parsing a symlink
    When parsing one or more directories of sources and a symlink was encountered,
    database construction would error out. The root cause was that Clink passed the
    symlink to Cscope as-is and Cscope refuses to parse symlinks. This behaviour of
    Cscope appears to be deliberate, so lets just work around it. When parsing a
    directory that contains symlinks, including the symlinked files seems the
    desirable behaviour.

--- test/cases/symlink-in-dir.c
+++ test/cases/symlink-in-dir.c
@@ -4,5 +4,4 @@
 /// that symlinks are resolved before we pass them to Cscope, even when found in
 /// a subdirectory of a source path.
 
-// XFAIL: True
 // RUN: clink --build-only --database={%t} --debug --parse-c=cscope {%S}/symlink >/dev/null
