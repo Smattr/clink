8a85923dd0ddf27f0862bc32b82228ccf531480a
Matthew Fernandez <matthew.fernandez@gmail.com>
2024-12-05 17:01:16 +1100

    add test cases for assignment finding
    Github: https://github.com/Smattr/clink/issues/231#issuecomment-1951450408

---
+++ test/cases/find-assignment-cscope.c
@@ -0,0 +1,11 @@
+/// can we identify assignments?
+
+int foo(void) {
+  int x = 0;
+  return x;
+}
+
+// XFAIL: True
+// RUN: clink --build-only --database={%t} --debug --parse-c=cscope {%s} >/dev/null
+// RUN: echo "select symbols.name, records.path, symbols.line from symbols inner join records on symbols.path = records.id where symbols.name = 'x' and symbols.category = 4;" | sqlite3 {%t}
+// CHECK: x|{%s}|4
