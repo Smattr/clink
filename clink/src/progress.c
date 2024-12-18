Smattr/clink

commit 1fa7eb65eeeb3d13dbced6c47b6c8e51dc9443a9

omit newline after progress bar
When constructing the database, this change removes the blank line at the bottom
of the output. When working in a small or multiplexed terminal, every little
line can be valuable.

clink/src/progress.c
@@ -49,13 +49,13 @@ static void update(unsigned long thread_id, char *line) {
 
   // move up to this thread’s progress line
   if (smart_progress())
-    printf("\033[%luA\033[K", option.threads - thread_id + 1);
+    printf("\033[%luF\033[K", option.threads - thread_id);
 
   printf("%lu: %s\n", thread_id, status[thread_id]);
 
   // move back to the bottom
   if (smart_progress()) {
-    printf("\033[%luB", option.threads - thread_id);
+    printf("\033[%luB", option.threads - thread_id - 1);
     fflush(stdout);
   }
 
@@ -99,7 +99,12 @@ static void progress(void) {
       }
     }
   }
-  printf("\n");
+  if (smart_progress()) {
+    printf("\033[K");
+    fflush(stdout);
+  } else {
+    printf("\n");
+  }
 }
 
 /** reconstruct the progress output that may have been overwritten
@@ -145,7 +150,7 @@ void progress_warn(unsigned long thread_id, const char *fmt, ...) {
 
   // move to the first line
   if (smart_progress())
-    printf("\033[%luA\033[K", option.threads + 1);
+    printf("\033[%luF\033[K", option.threads);
 
   // “warning: [thread <x>] ”
   if (option.colour == ALWAYS)
@@ -201,9 +206,9 @@ void progress_increment(void) {
 
   flockfile(stdout);
 
-  // move up to the line the progress is on
+  // move to the beginning of the line
   if (smart_progress())
-    printf("\033[1A\033[K");
+    printf("\033[1G");
 
   // update progress
   ++done;

commit d1ac2b7e18d46766dc1dc91d44aa7d46685294c6

fix progress output creeping downwards
1fa7eb65eeeb3d13dbced6c47b6c8e51dc9443a9 introduced a bug wherein the progress
output that was meant to remain fixed at a terminal offset would creep down the
terminal. This was not typically noticeable unless starting Clink in a fresh
terminal, with a lot of vertical space below. The problem is that '\033[0B'
actually moves down a line instead of leaving the cursor where it is. The
simplest solution seems to be to omit the trailing '\n', ensuring that we are
then always moving down ≥1 when moving back to the bottom.

clink/src/progress.c
@@ -67,14 +67,14 @@ static void update(unsigned long thread_id, char *line) {
     assert(my_digits <= most_digits);
     for (size_t j = my_digits; j < most_digits; ++j)
       putchar(' ');
-  }
 
-  printf("%lu: %s\n", thread_id, status[thread_id]);
+    printf("%lu: %s\033[K", thread_id, status[thread_id]);
 
-  // move back to the bottom
-  if (smart_progress()) {
-    printf("\033[%luB", option.threads - thread_id - 1);
+    // move back to the bottom
+    printf("\033[%luB", option.threads - thread_id);
     fflush(stdout);
+  } else {
+    printf("%lu: %s\n", thread_id, status[thread_id]);
   }
 
   funlockfile(stdout);