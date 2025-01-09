fc1a4fc6b734811ddd338a72316b06fadec8e0a6
Matthew Fernandez <matthew.fernandez@gmail.com>
2025-01-09 17:22:49 +1100

    fix: work around strict aliasing issues in arena allocation
    Technically dealing with the unallocated memory as a `unsigned char` array and
    then handing it out as arbitrarily typed pointers was UB, as I realised from a
    comment¹ linked on a blog post.² I think our usage here was fine because from
    this allocator we only ever allocate strings, `char *`s, that are then allowed
    to alias anything. But it seems wise to make this less error prone for future
    modification.
    
    ¹ https://www.reddit.com/r/C_Programming/comments/2cdpmd/comment/cjf3qhn
    ² https://tavianator.com/2014/alloca.html

--- libclink/src/arena.h
+++ libclink/src/arena.h
@@ -33,12 +33,20 @@ enum { CHUNK_SIZE = 16384 };
 /// The arena’s free list is stored as a linked-list of chunks from which to
 /// allocate memory. The amount of free space remaining in a chunk is stored at
 /// a higher level in the `arena_t` struct.
-typedef struct chunk {
+struct chunk {
   struct chunk *previous; ///< prior chunk in use
-  unsigned char
-      content[CHUNK_SIZE -
-              sizeof(struct chunk *)]; ///< backing memory to be allocated
-} chunk_t;
+  char content[CHUNK_SIZE -
+               sizeof(struct chunk *)]; ///< backing memory to be allocated
+};
+
+/// pointee type used for `struct chunk` pointers
+///
+/// The allocator itself wants to deal in `struct chunk`s but then hand out
+/// pointers to arbitrary types. While strict aliasing allows `char *` to alias
+/// other pointers it does not allow other pointers to alias `char[]`. So we
+/// cannot address allocatable memory as the `content` member of `struct chunk`
+/// but instead only refer to it through `chunk_t *`s.
+typedef char chunk_t;
 
 /// an arena’s state
 ///
--- libclink/src/arena_alloc.c
+++ libclink/src/arena_alloc.c
@@ -3,6 +3,7 @@
 #include <assert.h>
 #include <errno.h>
 #include <stdlib.h>
+#include <string.h>
 
 #ifdef __has_feature
 #if __has_feature(address_sanitizer)
@@ -23,6 +24,14 @@
   } while (0)
 #endif
 
+/// an arbitrary `struct chunk` on which to call `sizeof` on its fields
+///
+/// We want to use `sizeof(some_chunk.content)` below, but all chunks are
+/// addressed through `chunk_t *`s not `struct chunk *`s so this does not work.
+/// This object gives us a way to do it that does not cause UB like the usual
+/// tricks (`sizeof(((struct chunk *)0)->content)`).
+static const struct chunk witness;
+
 /// prepend a new chunk to allocate from
 ///
 /// \param me Arena to expand
@@ -31,16 +40,29 @@ static int add_chunk(arena_t *me) {
   assert(me != NULL);
 
   // create a new chunk
-  chunk_t *const next = calloc(1, sizeof(*next));
+  chunk_t *const next = calloc(1, sizeof(struct chunk));
   if (ERROR(next == NULL))
     return ENOMEM;
 
-  // posion the new, unallocated memory
+    // posion the new, unallocated memory
+#if 0
   POISON(next->content, sizeof(next->content));
+#else
+  POISON(next + offsetof(struct chunk, content), sizeof(witness.content));
+#endif
 
   // make it the start of the arena’s linked-list
+#if 0
   next->previous = me->current;
+#else
+  memcpy(next + offsetof(struct chunk, previous), &me->current,
+         sizeof(me->current));
+#endif
+#if 0
   *me = (arena_t){.remaining = sizeof(next->content), .current = next};
+#else
+  *me = (arena_t){.remaining = sizeof(witness.content), .current = next};
+#endif
 
   return 0;
 }
@@ -49,7 +71,7 @@ void *arena_alloc(arena_t *me, size_t size) {
   assert(me != NULL);
 
   // we cannot allocate something that will not fit in a chunk
-  if (ERROR(size > sizeof(me->current->content)))
+  if (ERROR(size > sizeof(witness.content)))
     return NULL;
 
   // if we do not have enough space, acquire a new chunk
@@ -60,7 +82,12 @@ void *arena_alloc(arena_t *me, size_t size) {
   assert(me->remaining >= size);
 
   // allocate from the end of the chunk for simplicity
+#if 0
   void *const answer = &me->current->content[me->remaining - size];
+#else
+  void *const answer =
+      me->current + offsetof(struct chunk, content) + me->remaining - size;
+#endif
   me->remaining -= size;
 
   // unpoison what we just allocated
