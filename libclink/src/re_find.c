125bdec16dda5cc12070a10af44c5e870b92fbeb
Matthew Fernandez <matthew.fernandez@gmail.com>
2025-04-18 17:07:01 +1000

    fix: use barriers when dealing with regex list
    Previously, the following sequence of actions could occur:
      1. Thread A load from `re` (re_add:38)
      2. Thread A cmpxchg to `re` (re_add:41)
      3. Thread B load from `re` (re_find:13)
      4. Thread B load from `&r->next` (re_find:17)
    
    At this point, writes to the fields of the new entry A added to the regex list
    may not be visible to B. That is, B’s `strcmp`, use of the compiled regex, or
    even reads of the `.next` field may see uninitialized memory or torn values.
    
    With `re_free`, this change is more of a paranoia step. It is assumed that
    thread joining presents a memory barrier itself.

--- libclink/src/re_find.c
+++ libclink/src/re_find.c
@@ -9,6 +9,9 @@ regex_t re_find(const re_t **re, const char *regex) {
   assert(re != NULL);
   assert(regex != NULL);
 
+  // synchronise with writes in `re_add`
+  __atomic_thread_fence(__ATOMIC_ACQUIRE);
+
   const re_t *r = __atomic_load_n(re, __ATOMIC_ACQUIRE);
   while (r != NULL) {
     if (strcmp(r->expression, regex) == 0)
--- libclink/src/re_free.c
+++ libclink/src/re_free.c
@@ -8,6 +8,9 @@ void re_free(void *re) {
 
   re_t **rep = re;
 
+  // synchronise with writes in `re_add`
+  __atomic_thread_fence(__ATOMIC_ACQUIRE);
+
   for (re_t *r = *rep; r != NULL; r = *rep) {
     free(r->expression);
     regfree(&r->compiled);
