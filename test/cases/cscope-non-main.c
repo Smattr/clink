c9fa32983a861c3b4f38b1820e1fbda36b56915c
Matthew Fernandez <matthew.fernandez@gmail.com>
2025-06-01 12:19:45 -0700

    add a test case for recognising '__old = …' in string.h
    This is a more general problem, but unfortunately I have not been able to come
    up with a self-contained test case. As a result, we have this fragile example
    which will only correctly probe the behaviour on certain versions of Glibc.

---
+++ test/cases/cscope-non-main.c
@@ -0,0 +1,16 @@
+/// will Cscope-based parsing incorrectly recognise assignments in the non-main
+/// file?
+
+// include a secondary file with some complex content
+#include <string.h>
+
+int main(void) {
+  int y = 42;
+  return 0;
+}
+
+// XFAIL: True
+// RUN: clink --build-only --database={%t} --debug --parse-c=cscope {%s} >/dev/null
+// RUN: echo "select name from symbols where name = '__old';" | sqlite3 {%t}
+// RUN: echo EOT
+// CHECK: EOT
