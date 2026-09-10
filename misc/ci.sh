2904406d85ccc0e7857e4ea258f8a93066b790e7
Matthew Fernandez <matthew.fernandez@gmail.com>
2026-09-09 18:17:50 -0700

    CI: migrate Linux job to Github Actions
    At this point, Cirrus CI is well and truly dead. Its domain is
    unreachable.

--- misc/ci.sh
+++ misc/ci.sh
@@ -17,10 +17,4 @@ cmake --build build --target check
 printf "find-me says: "
 ./build/test/find-me/find-me
 
-if [ "$(uname -s)" = "Darwin" ]; then
-  SUDO=sudo
-else
-  # sudo not needed
-  SUDO=
-fi
-${SUDO} cmake --install build
+sudo cmake --install build
